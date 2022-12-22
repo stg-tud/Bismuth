5597a749b00eb2f6c7d058c07d5c07de599c1ed4
Christian Kuessner <17302779+ckuessner@users.noreply.github.com>
2022-12-22 20:38:57 +0100

    itc: Add second generator for EventTrees

--- src/test/scala/causality/EventTreeGenerators.scala
+++ src/test/scala/causality/EventTreeGenerators.scala
@@ -1,7 +1,7 @@
 package de.tu_darmstadt.stg.daimpl
 package causality
 
-import causality.EventTree.seed
+import causality.EventTree.{Branch, Leaf, seed}
 import causality.IdTreeGenerators.genIdTree
 
 import org.scalacheck.{Arbitrary, Gen}
@@ -22,5 +22,29 @@ object EventTreeGenerators {
         ev <- modifyEventTree(eventTree, numModifications - 1)
       } yield ev.increment(id)
     } yield modifiedEventTree
+  }
+
+  given genRandomEventTree: Gen[EventTree] = {
+    for {
+      maxDepth  <- Gen.choose(1, 10)
+      eventTree <- genEventTreeBranch(maxDepth)
+    } yield eventTree
+  }
+
+  private def genEventTreeBranch(maxDepth: Int): Gen[Branch] = for {
+    maxDepthL      <- Gen.choose(0, maxDepth - 1)
+    maxDepthR      <- Gen.choose(0, maxDepth - 1)
+    branchValue    <- Gen.choose(0, 10)
+    leftEventTree  <- genEventTreeBranchOrLeaf(maxDepthL)
+    rightEventTree <- genEventTreeBranchOrLeaf(maxDepthR)
+  } yield Branch(branchValue, leftEventTree, rightEventTree)
+
+  val genEventTreeLeaf: Gen[Leaf] = for {
+    value <- Gen.choose(0, 100)
+  } yield Leaf(value)
+
+  private def genEventTreeBranchOrLeaf(maxDepth: Int): Gen[EventTree] = {
+    if (maxDepth == 0) genEventTreeLeaf
+    else Gen.oneOf(genEventTreeLeaf, genEventTreeBranch(maxDepth))
   }
 }
