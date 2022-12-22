b6c42165312996dc657fbcd3e6c09749e9126a8e
Christian Kuessner <17302779+ckuessner@users.noreply.github.com>
2022-12-22 20:39:29 +0100

    itc: Add more tests for EventTree

--- src/test/scala/causality/EventTreeTest.scala
+++ src/test/scala/causality/EventTreeTest.scala
@@ -3,12 +3,15 @@ package de.tu_darmstadt.stg.daimpl
 package causality
 
 import causality.EventTree.{Branch, Leaf, seed, given}
-import causality.EventTreeGenerators.genEventTree
+import causality.EventTreeGenerators.{genEventTree, genEventTreeLeaf, genRandomEventTree}
 
+import org.scalacheck.Gen
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.matchers.should.Matchers.shouldBe
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
+import scala.annotation.tailrec
 import scala.language.implicitConversions
 
 class EventTreeTest extends AnyFlatSpec with Matchers with ScalaCheckPropertyChecks {
@@ -16,6 +19,15 @@ class EventTreeTest extends AnyFlatSpec with Matchers with ScalaCheckPropertyChe
 
   implicit override val generatorDrivenConfig: PropertyCheckConfiguration =
     PropertyCheckConfiguration(maxDiscardedFactor = 20.0)
+
+  final def isNormalized(eventTree: EventTree): Boolean = eventTree match {
+    case Leaf(_)                         => true
+    case Branch(_, Leaf(0), r)           => isNormalized(r)
+    case Branch(_, Branch(0, ll, lr), r) => isNormalized(ll) && isNormalized(lr) && isNormalized(r)
+    case Branch(_, l, Leaf(0))           => isNormalized(l)
+    case Branch(_, l, Branch(0, rl, rr)) => isNormalized(l) && isNormalized(rl) && isNormalized(rr)
+    case _                               => false
+  }
 
   "Leaf" should "only accept positive values" in {
     assertThrows[IllegalArgumentException](Leaf(-1))
@@ -90,7 +102,16 @@ class EventTreeTest extends AnyFlatSpec with Matchers with ScalaCheckPropertyChe
   }
 
   it should "produce normalized Ids" in {
-    // TODO: implement this
+    forAll(genEventTree, genEventTreeLeaf) { (ev1, ev2) =>
+      val joined = ev1 join ev2
+      joined shouldBe joined.normalized
+    }
+  }
+
+  it should "be commutative" in {
+    forAll(genEventTree, genEventTree) { (ev1, ev2) =>
+      (ev1 join ev2) shouldBe (ev2 join ev1)
+    }
   }
 
   "increment" should "work" in {
@@ -111,9 +132,9 @@ class EventTreeTest extends AnyFlatSpec with Matchers with ScalaCheckPropertyChe
     }
   }
 
-  it should "be Some(1) or Some(-1) when a <= b but not b <= a or vice versa" in {
+  it should "be Some(1) or Some(-1) when a <= b xor b <= a" in {
     forAll(genEventTree, genEventTree) { (left, right) =>
-      whenever(left <= right || right <= left) {
+      whenever(left <= right ^ right <= left) {
         if (left <= right) {
           eventTreePord.tryCompare(left, right) shouldBe Some(-1)
           eventTreePord.tryCompare(right, left) shouldBe Some(1)
@@ -122,6 +143,69 @@ class EventTreeTest extends AnyFlatSpec with Matchers with ScalaCheckPropertyChe
           eventTreePord.tryCompare(right, left) shouldBe Some(-1)
         }
       }
+    }
+  }
+
+  "NormalForm[EventTree]" should "work for Leaves" in {
+    forAll(Gen.posNum[Int]) { (n: Int) =>
+      Leaf(n).normalized shouldBe Leaf(n)
+    }
+  }
+
+  it should "work for Branches" in {
+    forAll(Gen.posNum[Int], Gen.posNum[Int], Gen.posNum[Int]) { (value: Int, leftLeafValue: Int, rightLeafValue: Int) =>
+      val minLeaf = Math.min(leftLeafValue, rightLeafValue)
+      val normalizedEventTree = Branch(
+        value,
+        Leaf(leftLeafValue),
+        Leaf(rightLeafValue)
+      ).normalized
+      if (leftLeafValue == rightLeafValue) {
+        normalizedEventTree shouldBe Leaf(value + leftLeafValue)
+      } else {
+        normalizedEventTree shouldBe Branch(
+          value + minLeaf,
+          leftLeafValue - minLeaf,
+          rightLeafValue - minLeaf
+        )
+      }
+    }
+  }
+
+  it should "work for nested examples" in {
+    val nestedEventTrees = Table(
+      ("event", "expectedNormalForm"),
+      Branch(0, Branch(0, 0, 0), Leaf(0))           -> Leaf(0),
+      Branch(2, Branch(8, 9, 10), Branch(3, 3, 10)) -> Branch(8, Branch(11, 0, 1), Branch(0, 0, 7)),
+      Branch(2, Branch(8, 0, 10), Branch(3, 3, 10)) -> Branch(8, Branch(2, 0, 10), Branch(0, 0, 7)),
+      Branch(2, Leaf(3), Branch(3, 3, 10))          -> Branch(5, Leaf(0), Branch(3, 0, 7)),
+      Branch(3, Branch(2, Branch(2, 2, 2), Leaf(4)), Branch(0, Leaf(6), Branch(0, 6, 6))) -> Leaf(9)
+    )
+
+    forAll(nestedEventTrees) { (id, expectedNormalForm) =>
+      id.normalized shouldBe expectedNormalForm
+    }
+  }
+
+  it should "produce event trees that are normalized" in {
+    forAll(genEventTree) { ev =>
+      isNormalized(ev.normalized)
+    }
+
+    forAll(genRandomEventTree) { ev =>
+      isNormalized(ev.normalized)
+    }
+  }
+
+  it should "return `this` reference if already normalized" in {
+    forAll(genEventTree) { evTree =>
+      val normalizedEv = evTree.normalized
+      assert(normalizedEv eq normalizedEv.normalized)
+    }
+
+    forAll(genRandomEventTree) { evTree =>
+      val normalizedEv = evTree.normalized
+      assert(normalizedEv eq normalizedEv.normalized)
     }
   }
 }
