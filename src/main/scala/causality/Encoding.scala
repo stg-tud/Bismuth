9f3937b6c0bdd3ef720bf7535b48140b8dc942b8
Daniel Dirks <dandirks@web.de>
2023-01-05 15:52:46 +0100

    itc: Update Encoding to use BigInt bit representation

--- src/main/scala/causality/Encoding.scala
+++ src/main/scala/causality/Encoding.scala
@@ -3,17 +3,19 @@ package causality
 
 import scala.math.pow
 
-case class Digit(num: Int, bits: Int) {
-  def toBits(): String =
-    String.format("%" + bits + "s", num.toBinaryString).replace(' ', '0')
+case class Encoding(bits: BigInt, digits: Int) {
+  def addBits(n: Int, d: Int): Encoding = Encoding((bits << d) | n, digits + d)
+  def popBits(d: Int): (BigInt, Encoding) = (bits & BigDecimal(pow(2, d+1)-1).toBigInt, Encoding((bits >> d), digits - d))
+
+  def +(other: Encoding): Encoding = Encoding((bits << other.digits) | other.bits, digits + other.digits)
+
+  override def toString(): String = String.format("%1$" + digits + "s", bits.toString(2)).replace(' ', '0');
+  def toByteArray(): Array[Byte] = bits.toByteArray
+  def length(): Int = digits
 }
 
-case class Encoding(digits: Digit*) {
-  def +(other: Encoding): Encoding =
-    Encoding(digits ++ other.digits: _*)
-
-  def toBits(): String =
-    digits.map(_.toBits()).mkString("")
+object Encoding {
+  def apply(): Encoding = Encoding(0, 0)
 }
 
 object Encoder {
@@ -22,30 +24,31 @@ object Encoder {
 
   def encodeId(idTree: IdTree): Encoding = {
     import causality.IdTree.{Branch, Leaf}
-    idTree match 
-      case Leaf(0)                  => Encoding(Digit(0, 2), Digit(0, 1))
-      case Leaf(1)                  => Encoding(Digit(0, 2), Digit(1, 1))
-      case Branch(Leaf(0), i      ) => Encoding(Digit(1, 2)) + encodeId(i)
-      case Branch(i      , Leaf(0)) => Encoding(Digit(2, 2)) + encodeId(i)
-      case Branch(i1     , i2     ) => Encoding(Digit(3, 2)) + encodeId(i1) + encodeId(i2)
+    idTree match
+      case Leaf(0)                  => Encoding().addBits(0, 2).addBits(0, 1)
+      case Leaf(1)                  => Encoding().addBits(0, 2).addBits(1, 1)
+      case Branch(Leaf(0), i      ) => Encoding().addBits(1, 2) + encodeId(i)
+      case Branch(i      , Leaf(0)) => Encoding().addBits(2, 2) + encodeId(i)
+      case Branch(i1     , i2     ) => Encoding().addBits(3, 2) + encodeId(i1) + encodeId(i2)
   }
 
-  def encodeEvents(eventTree: EventTree): Encoding =
+  def encodeEvents(eventTree: EventTree): Encoding = {
     import causality.EventTree.{Branch, Leaf}
-    eventTree match 
-      case Branch(0, Leaf(0), e      ) => Encoding(Digit(0, 1), Digit(0, 2)) + encodeEvents(e)
-      case Branch(0, e      , Leaf(0)) => Encoding(Digit(0, 1), Digit(1, 2)) + encodeEvents(e)
-      case Branch(0, e1     , e2     ) => Encoding(Digit(0, 1), Digit(2, 2)) + encodeEvents(e1) + encodeEvents(e2)
-      case Branch(n, Leaf(0), e      ) => Encoding(Digit(0, 1), Digit(3, 2), Digit(0, 1), Digit(0, 1)) + encodeNum(n, 2) + encodeEvents(e)
-      case Branch(n, e      , Leaf(0)) => Encoding(Digit(0, 1), Digit(3, 2), Digit(0, 1), Digit(1, 1)) + encodeNum(n, 2) + encodeEvents(e)
-      case Branch(n, e1     , e2     ) => Encoding(Digit(0, 1), Digit(3, 2), Digit(1, 1)) + encodeNum(n, 2) + encodeEvents(e1) + encodeEvents(e2)
-      case Leaf(n)                     => Encoding(Digit(1, 1)) + encodeNum(n, 2)
+    eventTree match
+      case Branch(0, Leaf(0), e      ) => Encoding().addBits(0, 1).addBits(0, 2) + encodeEvents(e)
+      case Branch(0, e      , Leaf(0)) => Encoding().addBits(0, 1).addBits(1, 2) + encodeEvents(e)
+      case Branch(0, e1     , e2     ) => Encoding().addBits(0, 1).addBits(2, 2) + encodeEvents(e1) + encodeEvents(e2)
+      case Branch(n, Leaf(0), e      ) => Encoding().addBits(0, 1).addBits(3, 2).addBits(0, 1).addBits(0, 1) + encodeNum(n, 2) + encodeEvents(e)
+      case Branch(n, e      , Leaf(0)) => Encoding().addBits(0, 1).addBits(3, 2).addBits(0, 1).addBits(1, 1) + encodeNum(n, 2) + encodeEvents(e)
+      case Branch(n, e1     , e2     ) => Encoding().addBits(0, 1).addBits(3, 2).addBits(1, 1) + encodeNum(n, 2) + encodeEvents(e1) + encodeEvents(e2)
+      case Leaf(n)                     => Encoding().addBits(1, 1) + encodeNum(n, 2)
+  }
 
   def encodeNum(n: Int, B: Int): Encoding = {
     if (n < pow(2, B)) {
-      return Encoding(Digit(0, 1), Digit(n, B))
+      return Encoding().addBits(0, 1).addBits(n, B)
     } else {
-      return Encoding(Digit(1, 1)) + encodeNum(n - pow(2, B).intValue(), B+1)
+      return Encoding().addBits(1, 1) + encodeNum(n - pow(2, B).intValue(), B+1)
     }
   }
 }
--- src/test/scala/causality/EncodingTest.scala
+++ src/test/scala/causality/EncodingTest.scala
@@ -5,23 +5,17 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers.shouldBe
 
 class EncodingTest extends AnyFlatSpec {
-  "toBits" should "work for Digit" in {
-    assert(Digit(0, 1).toBits() == "0")
-    assert(Digit(1, 1).toBits() == "1")
-    assert(Digit(2, 3).toBits() == "010")
+  "toString" should "work for Encoding" in {
+    assert(Encoding().addBits(0, 1).toString() == "0")
+    assert(Encoding().addBits(1, 1).toString() == "1")
+    assert(Encoding().addBits(2, 3).toString() == "010")
+    assert(Encoding().addBits(1, 2).addBits(2, 2).toString() == "0110")
   }
 
-  it should "work for Encoding" in {
-    assert(Encoding(Digit(1, 2), Digit(2, 2)).toBits() == "0110")
-  }
-
-  "add" should "work for two digits" in {
-    assert(Encoding(Digit(0, 1)) + Encoding(Digit(1, 1)) == Encoding(Digit(0, 1), Digit(1, 1)))
-  }
-
-  it should "work for more digits" in {
-    assert(Encoding(Digit(0, 1), Digit(1, 1)) + Encoding(Digit(0, 1)) == Encoding(Digit(0, 1), Digit(1, 1), Digit(0, 1)))
-    assert(Encoding(Digit(0, 1), Digit(1, 1)) + Encoding(Digit(1, 1), Digit(0, 1)) == Encoding(Digit(0, 1), Digit(1, 1), Digit(1, 1), Digit(0, 1)))
+  "add" should "work for Encoding" in {
+    assert(Encoding().addBits(0, 1) + Encoding().addBits(1, 1) == Encoding().addBits(0, 1).addBits(1, 1))
+    assert(Encoding().addBits(0, 1).addBits(1, 1) + Encoding().addBits(0, 1) == Encoding().addBits(0, 1).addBits(1, 1).addBits(0, 1))
+    assert(Encoding().addBits(0, 1).addBits(1, 1) + Encoding().addBits(1, 1).addBits(0, 1) == Encoding().addBits(0, 1).addBits(1, 1).addBits(1, 1).addBits(0, 1))
   }
 
   it should "work for empty encodings" in {
@@ -29,86 +23,86 @@ class EncodingTest extends AnyFlatSpec {
   }
 
   "encodeId" should "encode Leaf(0)" in {
-    assert(Encoder.encodeId(IdTree.Leaf(0)).toBits() == "000")
+    assert(Encoder.encodeId(IdTree.Leaf(0)).toString() == "000")
   }
 
   it should "encode Leaf(1)" in {
-    assert(Encoder.encodeId(IdTree.Leaf(1)).toBits() == "001")
+    assert(Encoder.encodeId(IdTree.Leaf(1)).toString() == "001")
   }
 
   it should "encode right subtree" in {
-    assert(Encoder.encodeId(IdTree.Branch(IdTree.Leaf(0), IdTree.Leaf(1))).toBits() == "01001")
+    assert(Encoder.encodeId(IdTree.Branch(IdTree.Leaf(0), IdTree.Leaf(1))).toString() == "01001")
   }
 
   it should "encode left subtree" in {
-    assert(Encoder.encodeId(IdTree.Branch(IdTree.Leaf(1), IdTree.Leaf(0))).toBits() == "10001")
+    assert(Encoder.encodeId(IdTree.Branch(IdTree.Leaf(1), IdTree.Leaf(0))).toString() == "10001")
   }
 
   it should "encode both subtree" in {
-    assert(Encoder.encodeId(IdTree.Branch(IdTree.Leaf(1), IdTree.Leaf(1))).toBits() == "11001001")
+    assert(Encoder.encodeId(IdTree.Branch(IdTree.Leaf(1), IdTree.Leaf(1))).toString() == "11001001")
   }
 
   "encodeEvents" should "encode Leaf(0)" in {
-    assert(Encoder.encodeEvents(EventTree.Leaf(0)).toBits() == "1000")
+    assert(Encoder.encodeEvents(EventTree.Leaf(0)).toString() == "1000")
   }
 
   it should "encode Leaf(1)" in {
-    assert(Encoder.encodeEvents(EventTree.Leaf(1)).toBits() == "1001")
+    assert(Encoder.encodeEvents(EventTree.Leaf(1)).toString() == "1001")
   }
 
   it should "encode Leaf(42)" in {
-    assert(Encoder.encodeEvents(EventTree.Leaf(42)).toBits() == "1111001110")
+    assert(Encoder.encodeEvents(EventTree.Leaf(42)).toString() == "1111001110")
   }
 
   it should "encode right subtree when base is 0" in {
-    assert(Encoder.encodeEvents(EventTree.Branch(0, EventTree.Leaf(0), EventTree.Leaf(1))).toBits() == "0001001")
+    assert(Encoder.encodeEvents(EventTree.Branch(0, EventTree.Leaf(0), EventTree.Leaf(1))).toString() == "0001001")
   }
 
   it should "encode left subtree when base is 0" in {
-    assert(Encoder.encodeEvents(EventTree.Branch(0, EventTree.Leaf(1), EventTree.Leaf(0))).toBits() == "0011001")
+    assert(Encoder.encodeEvents(EventTree.Branch(0, EventTree.Leaf(1), EventTree.Leaf(0))).toString() == "0011001")
   }
 
   it should "encode both subtrees when base is 0" in {
-    assert(Encoder.encodeEvents(EventTree.Branch(0, EventTree.Leaf(1), EventTree.Leaf(1))).toBits() == "01010011001")
+    assert(Encoder.encodeEvents(EventTree.Branch(0, EventTree.Leaf(1), EventTree.Leaf(1))).toString() == "01010011001")
   }
 
   it should "encode right subtree" in {
-    assert(Encoder.encodeEvents(EventTree.Branch(1, EventTree.Leaf(0), EventTree.Leaf(1))).toBits() == "011000011001")
+    assert(Encoder.encodeEvents(EventTree.Branch(1, EventTree.Leaf(0), EventTree.Leaf(1))).toString() == "011000011001")
   }
 
   it should "encode left subtree" in {
-    assert(Encoder.encodeEvents(EventTree.Branch(1, EventTree.Leaf(1), EventTree.Leaf(0))).toBits() == "011010011001")
+    assert(Encoder.encodeEvents(EventTree.Branch(1, EventTree.Leaf(1), EventTree.Leaf(0))).toString() == "011010011001")
   }
 
   it should "encode both subtrees" in {
-    assert(Encoder.encodeEvents(EventTree.Branch(1, EventTree.Leaf(1), EventTree.Leaf(1))).toBits() == "011100110011001")
+    assert(Encoder.encodeEvents(EventTree.Branch(1, EventTree.Leaf(1), EventTree.Leaf(1))).toString() == "011100110011001")
   }
 
   "encodeNum" should "encode 0" in {
-    assert(Encoder.encodeNum(0, 1).toBits() == "00")
-    assert(Encoder.encodeNum(0, 3).toBits() == "0000")
+    assert(Encoder.encodeNum(0, 1).toString() == "00")
+    assert(Encoder.encodeNum(0, 3).toString() == "0000")
   }
 
   it should "encode 1" in {
-    assert(Encoder.encodeNum(1, 1).toBits() == "01")
-    assert(Encoder.encodeNum(1, 3).toBits() == "0001")
+    assert(Encoder.encodeNum(1, 1).toString() == "01")
+    assert(Encoder.encodeNum(1, 3).toString() == "0001")
   }
 
   it should "encode 42" in {
-    assert(Encoder.encodeNum(42, 2).toBits() == "111001110")
+    assert(Encoder.encodeNum(42, 2).toString() == "111001110")
   }
 
   it should "work with enough digits" in {
-    assert(Encoder.encodeNum(10, 4).toBits() == "01010")
+    assert(Encoder.encodeNum(10, 4).toString() == "01010")
   }
 
   it should "work with few digits" in {
-    assert(Encoder.encodeNum(10, 2).toBits() == "10110")
+    assert(Encoder.encodeNum(10, 2).toString() == "10110")
   }
 
   "encode" should "work for itc with zeros" in {
     val idTree = IdTree.Leaf(0)
     val eventTree = EventTree.Leaf(0)
-    assert(Encoder.encode(idTree, eventTree).toBits() == "0001000")
+    assert(Encoder.encode(idTree, eventTree).toString() == "0001000")
   }
 }
