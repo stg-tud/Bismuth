f6196a8ac8e781888ff883cde3d67e892135c2ed
Christian Kuessner <17302779+ckuessner@users.noreply.github.com>
2023-03-07 21:34:48 +0100

    itc: Add initial replica count to benchmark, refactoring

--- benchmarks/src/main/scala/causality/microbenchmarks/ForkEventJoinSendReceiveBenchmark.scala
+++ benchmarks/src/main/scala/causality/microbenchmarks/ForkEventJoinSendReceiveBenchmark.scala
@@ -9,124 +9,27 @@ import causality.{EventTree, ForkEventJoinClock, IdTree, IntervalTreeClock}
 
 import org.openjdk.jmh.annotations.*
 
+import scala.collection.mutable
 import scala.reflect.ClassTag
+import scala.runtime.Arrays
 import scala.util.Random
 
-@Warmup(time = 1)
+@Warmup(time = 1, iterations = 3)
 @Measurement(time = 1, iterations = 3)
 @Fork(1)
 @BenchmarkMode(Array(Mode.AverageTime))
 class ForkEventJoinSendReceiveBenchmark {
   @Benchmark
   def testIntervalTreeClock(program: ForkEventJoinProgram): Unit = {
-    runProgram(program, IntervalTreeClock.Clock.seed)
+    val runner = new ProgramRunner(program, program.itcReplicas)
+    runner.runProgram()
   }
 
   @Benchmark
   def testVectorClock(program: ForkEventJoinProgram): Unit = {
-    runProgram(program, VectorClock.Clock.seed)
+    val runner = new ProgramRunner(program, program.vcReplicas)
+    runner.runProgram()
   }
 
-  private inline def runProgram[T: ForkEventJoinClock: ClassTag](
-      program: ForkEventJoinProgram,
-      seedStamp: T
-  ): Unit = {
-    // TODO: Add encoding and decoding to send receive once implemented for ITC
-    val replicas = Array.ofDim[T](program.maxNumReplicas)
-    replicas(0) = seedStamp
-    var numReplicas = 1
 
-    program.commands.foreach {
-      case EventCmd(replica) =>
-        replicas(replica) = replicas(replica).event
-      case ForkCmd(replica) =>
-        val (existingReplica, newReplica) = replicas(replica).fork
-        replicas(replica) = existingReplica
-        replicas(numReplicas) = newReplica
-        numReplicas += 1
-      case JoinCmd(replica1, replica2) =>
-        replicas(replica1) = replicas(replica1).join(replicas(replica2))
-        // Delete replica entry by swapping with last replica and decrementing numReplicas
-        replicas(replica2) = replicas(numReplicas - 1)
-        numReplicas -= 1
-      case SendAndReceiveCmd(replica1, replica2) =>
-        val (localStamp, stampToSend) = replicas(replica1).send
-        replicas(replica1) = localStamp
-        replicas(replica2) = replicas(replica2).receive(stampToSend)
-      case _ =>
-        ???
-    }
-  }
 }
-
-@State(Scope.Thread)
-class ForkEventJoinProgram {
-  // Event, Fork, Join, Send followed by Receive
-  @Param(Array("25,25,25,25"))
-  var _probabilities: String = ""
-  lazy val probabilities: Array[Int] = {
-    _probabilities.split(',').map(_.toInt)
-  }
-
-  @Param(Array("1000", "10000", "100000"))
-  var programLength: Int = 0
-
-  var commands: Array[ForkEventJoinCommand] = Array.empty
-  var maxNumReplicas                        = 0
-
-  private val random = Random(42) // We want this to be deterministic
-
-  @Setup
-  def buildProgram(): Unit = {
-    val program = Array.ofDim[ForkEventJoinCommand](programLength)
-    var progIdx = 0
-
-    val hundredPercent     = probabilities.sum
-    val decisionBoundaries = probabilities.scanLeft(0)((a, b) => a + b)
-
-    var numReplicas = 1
-
-    while (progIdx < programLength) {
-      val decision = random.nextInt(hundredPercent)
-      // Replica to perform command on
-      val performingReplica = random.nextInt(numReplicas)
-      if (decision < decisionBoundaries(0)) { // Event
-        program(progIdx) = EventCmd(performingReplica)
-        progIdx += 1
-      } else if (decision < decisionBoundaries(1)) { // Fork
-        program(progIdx) = ForkCmd(performingReplica)
-        numReplicas += 1
-        maxNumReplicas = Math.max(maxNumReplicas, numReplicas)
-        progIdx += 1
-      } else if (decision < decisionBoundaries(2)) { // Join
-        // Check if we actually have 2 or more replicas to join
-        if (numReplicas >= 2) {
-          var otherReplica = performingReplica
-          while (otherReplica == performingReplica) {
-            otherReplica = random.nextInt(numReplicas)
-          }
-          program(progIdx) = JoinCmd(performingReplica, otherReplica)
-          numReplicas -= 1
-          progIdx += 1
-        }
-      } else if (decision < decisionBoundaries(3)) { // Send and receive
-        if (numReplicas >= 2) {
-          var receiver = performingReplica
-          while (receiver == performingReplica) {
-            receiver = random.nextInt(numReplicas)
-          }
-          program(progIdx) = SendAndReceiveCmd(performingReplica, receiver)
-          progIdx += 1
-        }
-      }
-    }
-
-    commands = program
-  }
-}
-
-trait ForkEventJoinCommand
-case class EventCmd(replica: Int)                          extends ForkEventJoinCommand
-case class ForkCmd(replica: Int)                           extends ForkEventJoinCommand
-case class JoinCmd(replica1: Int, replica2: Int)           extends ForkEventJoinCommand
-case class SendAndReceiveCmd(replica1: Int, replica2: Int) extends ForkEventJoinCommand
---
+++ benchmarks/src/main/scala/causality/microbenchmarks/ProgramRunner.scala
@@ -0,0 +1,31 @@
+package de.tu_darmstadt.stg.daimpl
+package causality.microbenchmarks
+
+import causality.ForkEventJoinClock
+
+class ProgramRunner[T: ForkEventJoinClock](program: ForkEventJoinProgram, val replicas: Array[T]) {
+  var numReplicas = 1
+
+  def runProgram(): Unit = {
+    program.commands.foreach(command => exec(command))
+  }
+
+  private inline def exec(cmd: ForkEventJoinCommand): Unit = cmd match {
+    case EventCmd(replica) =>
+      replicas(replica) = replicas(replica).event
+    case ForkCmd(replica) =>
+      val (existingReplica, newReplica) = replicas(replica).fork
+      replicas(replica) = existingReplica
+      replicas(numReplicas) = newReplica
+      numReplicas += 1
+    case JoinCmd(replica1, replica2) =>
+      replicas(replica1) = replicas(replica1).join(replicas(replica2))
+      // Delete replica entry by swapping with last replica and decrementing numReplicas
+      replicas(replica2) = replicas(numReplicas - 1)
+      numReplicas -= 1
+    case SendAndReceiveCmd(replica1, replica2) =>
+      val (localStamp, stampToSend) = replicas(replica1).send
+      replicas(replica1) = localStamp
+      replicas(replica2) = replicas(replica2).receive(stampToSend)
+  }
+}
