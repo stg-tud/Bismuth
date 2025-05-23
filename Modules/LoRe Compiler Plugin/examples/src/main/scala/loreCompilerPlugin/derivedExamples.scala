22f3148e3be36a9addb52b1f8efd05ca9d6395e1
Robin Breitenbach <robin.breitenbach@stud.tu-darmstadt.de>
2025-05-23 17:46:01 +0200

    Update example files

--- Modules/LoRe Compiler Plugin/examples/src/main/scala/loreCompilerPlugin/derivedExamples.scala
+++ Modules/LoRe Compiler Plugin/examples/src/main/scala/loreCompilerPlugin/derivedExamples.scala
@@ -6,34 +6,33 @@ import reactives.default.{Var as Source, Signal as Derived}
 
 def foo(bar: Int, baz: String): Int = 0
 
-object derivedExamplesObject:
-  @LoReProgram
-  def derivedExamplesFunction(): Unit = {
-    // ========= Plain values and sources to use in below tests =========
-    val integerLiteral: Int          = 1
-    val stringLiteral: String        = "foo"
-    val booleanLiteral: Boolean      = true
-    val functionCall: Int            = foo(1, "foobar")
-    val methodCallNoParens: Int      = 1.toInt
-    val methodCallWithParens: String = "foo".strip()
-    val methodCallWithParams: String = "foo".repeat(5)
+@LoReProgram
+object derivedExamples:
+  // ========= Plain values and sources to use in below tests =========
+  val integerLiteral: Int     = 1
+  val stringLiteral: String   = "foo"
+  val booleanLiteral: Boolean = true
 
-    val integerLiteralSource1: Source[Int]  = Source(1)
-    val integerLiteralSource2: Source[Int]  = Source(2)
-    val stringLiteralSource: Source[String] = Source("bar")
-    val boolLiteralSource: Source[Boolean]  = Source(false)
+  // The following 4 examples are supported in the frontend but not the backend
+//    val functionCall: Int            = foo(1, "foobar")
+//    val methodCallNoParens: Int      = 1.toInt
+//    val methodCallWithParens: String = "foo".strip()
+//    val methodCallWithParams: String = "foo".repeat(5)
 
-    // ========= Derived tests =========
+  val integerLiteralSource1: Source[Int]  = Source(1)
+  val integerLiteralSource2: Source[Int]  = Source(2)
+  val stringLiteralSource: Source[String] = Source("bar")
+  val boolLiteralSource: Source[Boolean]  = Source(false)
 
-    // These will have constant values, as they depend on values that are not mutable
-    val integerLiteralDerived: Derived[Int]    = Derived { 1 + 2 }
-    val integerReferenceDerived1: Derived[Int] = Derived { integerLiteral + 2 }
-    val integerReferenceDerived2: Derived[Int] = Derived { 1 + integerLiteral }
+  // ========= Derived tests =========
 
-    // Single and double dependencies on integer sources
-    val integerSourceDerived1: Derived[Int] = Derived { integerLiteralSource1.value + 2 }
-    val integerSourceDerived2: Derived[Int] = Derived { 1 + integerLiteralSource2.value }
-    val integerSourceDerived3: Derived[Int] = Derived { integerLiteralSource1.value + integerLiteralSource2.value }
+  // These will have constant values, as they depend on values that are not mutable
+  val integerLiteralDerived: Derived[Int]    = Derived { 1 + 2 }
+  val integerReferenceDerived1: Derived[Int] = Derived { integerLiteral + 2 }
+  val integerReferenceDerived2: Derived[Int] = Derived { 1 + integerLiteral }
 
-  }
+  // Single and double dependencies on integer sources
+  val integerSourceDerived1: Derived[Int] = Derived { integerLiteralSource1.value + 2 }
+  val integerSourceDerived2: Derived[Int] = Derived { 1 + integerLiteralSource2.value }
+  val integerSourceDerived3: Derived[Int] = Derived { integerLiteralSource1.value + integerLiteralSource2.value }
 end derivedExamplesObject
