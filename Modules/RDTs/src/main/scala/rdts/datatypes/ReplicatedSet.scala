67c9ac103eca654103a552038eabd8560b7e96a3
ragnar <git@rmgk.de>
2025-06-11 19:51:00 +0200

    flatten dotted into replicated set

--- Modules/RDTs/src/main/scala/rdts/datatypes/ReplicatedSet.scala
+++ Modules/RDTs/src/main/scala/rdts/datatypes/ReplicatedSet.scala
@@ -1,17 +1,13 @@
 package rdts.datatypes
 
-import rdts.base.{Bottom, Decompose, Lattice, LocalUid}
-import rdts.dotted.*
-import rdts.dotted.HasDots.mapInstance
+import rdts.base.{Bottom, Decompose, DecoratedLattice, Lattice, LocalUid}
 import rdts.time.{Dot, Dots}
 
 /** A set that allows deletes.
   * Each unique element tracks the dots of when it was inserted.
   * Removals do not override concurrent inserts.
   */
-case class ReplicatedSet[E](repr: Dotted[Map[E, Dots]]) {
-
-  def inner = repr.data
+case class ReplicatedSet[E](inner: Map[E, Dots], deleted: Dots) {
 
   type Delta = ReplicatedSet[E]
 
@@ -19,37 +15,30 @@ case class ReplicatedSet[E](repr: Dotted[Map[E, Dots]]) {
 
   def contains(elem: E): Boolean = inner.contains(elem)
 
+  lazy val observed = inner.values.foldLeft(deleted)(_ `union` _)
+
   def add(using LocalUid)(e: E): Delta = {
-    val nextDot = repr.context.nextDot(LocalUid.replicaId)
+    val nextDot = observed.nextDot(LocalUid.replicaId)
     val v: Dots = inner.getOrElse(e, Dots.empty)
 
-    deltaState(
-      dm = Map(e -> Dots.single(nextDot)),
-      cc = v `add` nextDot
-    )
+    ReplicatedSet(Map(e -> Dots.single(nextDot)), v)
   }
 
   def addAll(using LocalUid)(elems: Iterable[E]): Delta = {
-    val nextCounter = repr.context.nextTime(LocalUid.replicaId)
+    val nextCounter = observed.nextTime(LocalUid.replicaId)
     val nextDots    = Dots.from((nextCounter until nextCounter + elems.size).map(Dot(LocalUid.replicaId, _)))
 
-    val ccontextSet = elems.foldLeft(nextDots) {
-      case (dots, e) => inner.get(e) match {
-          case Some(ds) => dots `union` ds
-          case None     => dots
-        }
-    }
+    val ccontextSet = elems.flatMap(inner.get).foldLeft(Dots.empty)(_ `union` _)
 
-    deltaState(
-      dm = (elems zip nextDots.iterator.map(dot => Dots.single(dot))).toMap,
-      cc = ccontextSet
+    ReplicatedSet(
+      (elems zip nextDots.iterator.map(dot => Dots.single(dot))).toMap,
+      ccontextSet
     )
   }
 
   def remove(e: E): Delta = {
     val v = inner.getOrElse(e, Dots.empty)
-
-    deltaState(v)
+    ReplicatedSet(Map.empty, v)
   }
 
   def removeAll(elems: Iterable[E]): Delta = {
@@ -60,7 +49,7 @@ case class ReplicatedSet[E](repr: Dotted[Map[E, Dots]]) {
         }
     }
 
-    deltaState(dotsToRemove)
+    ReplicatedSet(Map.empty, dotsToRemove)
   }
 
   def removeBy(cond: E => Boolean): Delta = {
@@ -68,27 +57,22 @@ case class ReplicatedSet[E](repr: Dotted[Map[E, Dots]]) {
       case (k, v) if cond(k) => v
     }.foldLeft(Dots.empty)(_ `union` _)
 
-    deltaState(
-      cc = removedDots
-    )
+    ReplicatedSet(Map.empty, removedDots)
   }
 
-  def clear(): Delta = { deltaState(inner.dots) }
+  def clear(): Delta = ReplicatedSet(Map.empty, inner.values.foldLeft(Dots.empty)(_ `union` _))
 
-  private def deltaState(
-      dm: Map[E, Dots],
-      cc: Dots
-  ): Delta = ReplicatedSet(Dotted(dm, cc))
-
-  private def deltaState(cc: Dots): Delta = deltaState(Map.empty, cc)
 }
 
 object ReplicatedSet {
 
-  def empty[E]: ReplicatedSet[E] = ReplicatedSet(Dotted.empty)
+  def empty[E]: ReplicatedSet[E] = ReplicatedSet(Map.empty, Dots.empty)
 
-  given bottom[E]: Bottom[ReplicatedSet[E]]       = Bottom.provide(empty)
-  given lattice[E]: Lattice[ReplicatedSet[E]]     = Lattice.derived
+  given bottom[E]: Bottom[ReplicatedSet[E]]   = Bottom.provide(empty)
+  given lattice[E]: Lattice[ReplicatedSet[E]] = DecoratedLattice.filter[ReplicatedSet[E]](Lattice.derived) {
+    (base, other) =>
+      base.copy(inner = base.inner.filter((_, dots) => !other.deleted.subsumes(dots)))
+  }
   given decompose[E]: Decompose[ReplicatedSet[E]] = Decompose.derived
 
 }
