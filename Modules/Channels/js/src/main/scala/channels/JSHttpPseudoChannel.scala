0fe9d69d49353cc05c57858f7cb4c5af8e5b28bb
ragnar <git@rmgk.de>
2025-06-22 17:04:06 +0200

    figure out how aborting fetch actually works

--- Modules/Channels/js/src/main/scala/channels/JSHttpPseudoChannel.scala
+++ Modules/Channels/js/src/main/scala/channels/JSHttpPseudoChannel.scala
@@ -2,11 +2,12 @@ package channels
 
 import channels.MesageBufferExtensions.asArrayBuffer
 import de.rmgk.delay.{Async, Callback, toAsync}
-import org.scalajs.dom.{Headers, HttpMethod, ReadableStreamReader, RequestInit, Response, fetch}
+import org.scalajs.dom.{AbortController, Headers, HttpMethod, ReadableStreamReader, RequestInit, Response, fetch}
 
 import java.nio.ByteBuffer
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.scalajs.js.typedarray.{Int8Array, Uint8Array}
+import scala.util.Failure
 import scala.util.chaining.scalaUtilChainingOps
 
 object JSHttpPseudoChannel {
@@ -16,32 +17,36 @@ object JSHttpPseudoChannel {
 
     lazy val resultCallback: Callback[MessageBuffer] = receiver.messageHandler(this)
 
-    var currentConsumer: StreamConsumer | Null = null
+    var currentAbort: AbortController | Null = null
 
     override def send(message: MessageBuffer): Async[Any, Unit] = Async {
+
+      val abort = new AbortController()
 
       val requestInit = new RequestInit {}.tap: ri =>
         ri.method = HttpMethod.POST
         ri.body = message.asArrayBuffer
+        ri.signal = abort.signal
         ri.headers = Headers().tap: hi =>
           hi.set("Accept", "text/event-stream")
 
       val res = fetch(uri, requestInit).toFuture.toAsync.bind
 
-      handleResponses(res).bind
+      handleResponses(res, abort).bind
 
     }
 
-    def handleResponses(res: Response): Async[Any, Unit] = Async {
+    def handleResponses(res: Response, abort: AbortController): Async[Any, Unit] = Async {
 
       val reader = res.body.getReader()
 
-      val priorConsumer = currentConsumer
-      currentConsumer = StreamConsumer(reader, resultCallback)
-      if priorConsumer != null
-      then priorConsumer.close()
+      if currentAbort != null
+      then currentAbort.nn.abort()
+      currentAbort = abort
 
-      currentConsumer.nn.loop().run(using ())(_ => ())
+      val streamCon = StreamConsumer(reader, resultCallback)
+
+      streamCon.loop().run(using ()) { _ => () }
     }
 
     override def close(): Unit = ()
@@ -61,17 +66,20 @@ object JSHttpPseudoChannel {
     def loop(): Async[Any, Unit] = Async {
       val chunk = reader.read().toFuture.toAsync.bind
       val input = chunk.value
-      buffer = buffer.appendedAll(new Int8Array(input.buffer, input.byteOffset, input.length).toArray)
+      Async.bind {
+        if !scalajs.js.isUndefined(input) then {
+          buffer = buffer.appendedAll(new Int8Array(input.buffer, input.byteOffset, input.length).toArray)
 
-      if buffer.length >= 4 then
-        val len = ByteBuffer.wrap(buffer.slice(0, 4)).getInt()
-        if buffer.length >= len + 4 then
-          val mb = ArrayMessageBuffer(buffer.slice(4, len + 4))
-          buffer = buffer.slice(len + 4, buffer.length)
-          cb.succeed(mb)
+          if buffer.length >= 4 then
+            val len = ByteBuffer.wrap(buffer.slice(0, 4)).getInt()
+            if buffer.length >= len + 4 then
+              val mb = ArrayMessageBuffer(buffer.slice(4, len + 4))
+              buffer = buffer.slice(len + 4, buffer.length)
+              cb.succeed(mb)
 
-      loop().bind
-
+          loop()
+        } else Async(())
+      }
     }
 
   }
