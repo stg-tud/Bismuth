20831554aa7848c9487cc8a837913775e3dbf72e
David Scandurra <david.scandurra@check24.de>
2025-06-23 06:27:55 +0200

    Fix BasicTest

--- Modules/Tabular/lib/src/test/scala/com/daimpl/lib/BasicTest.scala
+++ Modules/Tabular/lib/src/test/scala/com/daimpl/lib/BasicTest.scala
@@ -12,7 +12,7 @@ class BasicTest extends munit.FunSuite {
 
     agg.edit(_.editCell(0, 0, "test"))
 
-    assertEquals(agg.current.read(0, 0), Some("test"))
+    assertEquals(agg.current.read(0, 0), Set("test"))
 
     agg.edit(_.removeColumn(0))
 
