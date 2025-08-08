3885866210e50a471fc6b91d7e02ba02c6080921
Hamza <hamza.wardi@stud.tu-darmstadt.de>
2025-08-08 21:31:25 +0200

    add test to BFT/Counter

--- Modules/BFT/src/test/scala/CounterTest.scala
+++ Modules/BFT/src/test/scala/CounterTest.scala
@@ -6,7 +6,7 @@ class CounterTest extends munit.FunSuite:
     val counterA1 = Counter()
     val (counterA2, event1) = counterA1.add(1)
 
-    assert(counterA2.value == 1, "event creation failed")
+    assert(counterA2.value == 1)
 
   }
 
@@ -15,11 +15,11 @@ class CounterTest extends munit.FunSuite:
     val (counterA2, event1) = counterA1.add(1)
     val (counterA3, event2) = counterA2.add(2)
 
-    assert(counterA3.value == 3, "event creation failed")
+    assert(counterA3.value == 3)
 
   }
 
-  test("sync two counters") {
+  test("sync two counters: example 1") {
     val counterA1 = Counter()
     val (counterA2, event1) = counterA1.add(1)
 
@@ -29,6 +29,25 @@ class CounterTest extends munit.FunSuite:
     val counterA3 = counterA2.receiveEvent(event2)
     val counterB3 = counterB2.receiveEvent(event1)
 
-    assert(counterA3.value == counterB3.value, true)
+    assert(counterA3.value == 3)
+    assert(counterA3.value == counterB3.value)
+
+  }
+
+  test("sync two counters: example 2") {
+    val counterA1 = Counter()
+    val (counterA2, event1) = counterA1.add(1)
+
+    val counterB1 = Counter()
+    val (counterB2, event2) = counterB1.add(2)
+    val (counterB3, event3) = counterB2.add(-5)
+
+    val counterA3 = counterA2.receiveEvent(event2)
+    val counterB4 = counterB3.receiveEvent(event1)
+    val counterA4 = counterA3.receiveEvent(event3)
+
+
+    assert(counterA4.value == -2)
+    assert(counterA4.value == counterB4.value)
 
   }
