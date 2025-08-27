6e58f53f1ce1f47b8a4cd5991f0eacf83179aa27
Wardi, Hamza <hamzawerdi@stud.tu-darmstadt.de>
2025-08-27 18:50:32 +0200

    add equivocation detection to bft/HashDAG

--- Modules/BFT/src/main/scala/dag/HashDAG.scala
+++ Modules/BFT/src/main/scala/dag/HashDAG.scala
@@ -1,7 +1,7 @@
 package dag
 
 import scala.collection.immutable.{HashMap, Map, Set, List}
-import java.security.KeyPair
+import java.security.{KeyPair, PublicKey}
 import crypto.Ed25519Util
 
 // a hash directed acyclic graph
@@ -9,9 +9,10 @@ case class HashDAG[T] private(
                                // the state S of a HashDAG consists of the vertices and edges contained in the HashDAG, S = (V, E)
                                // but separately saving the vertices and edges is redundant,
                                // a better way to solve this is by mapping each vertex to the set of its children
-                               graph: Map[Event[T], Set[Event[T]]],
+                               graph: Map[Event[T], Set[String]],
                                authorKeys: KeyPair,
-                               queue: Set[Event[T]]
+                               queue: Set[Event[T]] = Set.empty[Event[T]],
+                               byzantineNodes: Set[PublicKey] = Set.empty
                              ):
 
   // checks if an event is contained in the graph
@@ -19,6 +20,9 @@ case class HashDAG[T] private(
 
   def contains(id: String): Boolean =
     graph.exists((e, _) => e.id == id)
+
+  def contains(node: PublicKey): Boolean =
+    graph.exists((e, _) => e.author == node)
 
   // checks for the existence of child vertices
   def hasChild(event: Event[T]): Boolean =
@@ -34,26 +38,28 @@ case class HashDAG[T] private(
     val currentHeads = getCurrentHeads
     val currentHeadsIDs = currentHeads.map(event => event.id)
     val signature = Ed25519Util.sign(content.toString.getBytes, authorKeys.getPrivate)
-    
+
     Event(Some(content), authorKeys.getPublic, currentHeadsIDs, signature)
   }
 
   def effector(event: Event[T]): HashDAG[T] =
     assume(!contains(event), "Event already in HashDAG!")
-
-    if event.calculateHash != event.id || !event.signatureIsValid then
-      return this
-
-    val dependencies = event.dependencies
-    if dependencies.forall(e => contains(e)) then
-      var g = this.graph
-      for (d <- dependencies)
-        val e = g.find((e, _) => e.id == d).get._1
-        g = g.updated(e, g(e) + event)
-        
-      HashDAG(g + (event -> Set.empty), this.authorKeys, this.queue - event)
+    
+    if event.calculateHash != event.id || !event.signatureIsValid || event.authorIsByzantine then
+      this
+    else if isByzantine(event) then
+      markEventsFromByzantineNode(event.author)
     else
-      HashDAG(this.graph, this.authorKeys, this.queue + event)
+      val dependencies = event.dependencies
+      if dependencies.forall(e => contains(e)) then
+        var g = this.graph
+        for (d <- dependencies)
+          val e = g.find((e, _) => e.id == d).get._1
+          g = g.updated(e, g(e) + event.id)
+
+        HashDAG(g + (event -> Set.empty), this.authorKeys, this.queue - event)
+      else
+        HashDAG(this.graph, this.authorKeys, this.queue + event)
 
 
   def addEvent(content: T): HashDAG[T] =
@@ -63,31 +69,51 @@ case class HashDAG[T] private(
 
     // apply the event
     effector(event)
-    
+
   def processQueue(): HashDAG[T] =
     var hashDAG = this
-    for (event <- this.queue) 
-      hashDAG = effector(event)
-      
+    var events = Set.empty[Event[T]]
+
+    for (event <- this.queue) {
+      val tmp = hashDAG.effector(event)
+      hashDAG = tmp
+    }
+
     hashDAG
-    
+
   def getEventByID(id: String): Event[T] =
     graph.find((e, _) => e.id == id).get._1
-    
-  def pathExists(from: Event[T], to: Event[T], visited: Set[Event[T]] = Set()): Boolean = 
-    def dfs(current: Event[T], visited: Set[Event[T]]): Boolean = 
+
+  def pathExists(from: String, to: String, visited: Set[String] = Set()): Boolean =
+    def dfs(current: String, visited: Set[String]): Boolean =
       if (current == to) true
       else if (visited.contains(current)) false
-      else 
-        val neighbours = graph.getOrElse(current, Nil)
+      else
+        val neighbours = graph.getOrElse(getEventByID(current), Nil)
         neighbours.exists(neighbor => dfs(neighbor, visited + current))
 
     dfs(from, Set())
 
+  def isByzantine(event: Event[T]): Boolean =
+    if byzantineNodes.contains(event.author) then
+      true
+    else
+      // detect equivocation
+      var isByz = false
+      for (otherEvent <- graph.keys)
+        if otherEvent.author == event.author && event.dependencies.intersect(otherEvent.dependencies).nonEmpty then
+          isByz = true
+
+      isByz
+      
+  
+  def markEventsFromByzantineNode(author: PublicKey): HashDAG[T] = ???
+    
+
 
 object HashDAG:
   def apply[T](authorKeys: KeyPair): HashDAG[T] =
-    val graph = new HashMap[Event[T], Set[Event[T]]]()
+    val graph = new HashMap[Event[T], Set[String]]()
     val root = new Event("0", None, authorKeys.getPublic, Set.empty, Array.empty).asInstanceOf[Event[T]]
 
-    new HashDAG[T](graph.updated(root, Set.empty), authorKeys, Set.empty)
+    new HashDAG[T](graph.updated(root, Set.empty), authorKeys)
