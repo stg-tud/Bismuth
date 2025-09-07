c097f9ac5f3ce317cf390c0f4eeb02619c7b44f5
ragnar <git@rmgk.de>
2025-09-07 23:08:18 +0100

    more dare

--- Modules/Examples JVM/src/main/scala/ex2025protocols/dare.scala
+++ Modules/Examples JVM/src/main/scala/ex2025protocols/dare.scala
@@ -1,9 +1,19 @@
+//> using scala 3.7.2
+//> using dep "de.tu-darmstadt.stg::rdts:0.37.0+651-afc47005"
+//> using dep "com.lihaoyi::pprint:0.9.3"
+
 package ex2025protocols
 
 import rdts.base.{Lattice, LocalUid}
-import rdts.datatypes.{GrowOnlyCounter, PosNegCounter}
+import rdts.datatypes.{GrowOnlyCounter, LastWriterWins}
 import rdts.syntax.{DeltaBuffer, DeltaBufferContainer}
 
+/** For this lecuter/exercise, we are going to explore an executable model of a distributed system,
+  * consisting of many of these replicas. Each replica has its own current state, and a replica ID.
+  * There is no actual network involved, and it is just a helper
+  * to make exploring possible behaviour of a distributed system possible.
+  * It has no value in a real program.
+  */
 class Replica[A](init: A) {
   val replicaId: LocalUid             = LocalUid.gen()
   val buffer: DeltaBufferContainer[A] = DeltaBuffer(init).mutable
@@ -35,18 +45,10 @@ object Replica {
 
 }
 
-import rdts.base.{Bottom, Lattice, LocalUid}
-import rdts.datatypes.LastWriterWins
-
-object MiniSocial {
-
-  given Lattice[MiniSocial] = Lattice.derived
-
-  // needed by the replication manager to handle some special cases
-  given bottom: Bottom[MiniSocial] = Bottom.provide(MiniSocial())
-
-}
-
+/** Mini Social models a social network … except that there is only a single global message which can be voted for.
+  * Also when changing the message, the votes stay, so be careful.
+  * Its not a useful thing, but it does model the potential of composition.
+  */
 case class MiniSocial(
     message: LastWriterWins[String] = LastWriterWins.fallback(""),
     upvotes: GrowOnlyCounter = GrowOnlyCounter.zero,
@@ -62,48 +64,25 @@ case class MiniSocial(
     MiniSocial(message = message.write(newMessage))
 }
 
+object MiniSocial {
+  given Lattice[MiniSocial] = Lattice.derived
+}
+
+
+/** The main objct, serving as kinda tests to play around with. */
 object Dare {
 
   def main(args: Array[String]): Unit = {
-//    {
-//      val init = PosNegCounter.zero
-//      // this means a, b, and c are all initilized by executing the RHS multiple times
-//      val a, b, c = Replica(init)
-//
-//      a.mod(_.inc())
-//      b.mod(_.inc())
-//      c.receive(a).receive(b)
-//      a.show()
-//      b.show()
-//      c.show()
-//
-//    }
-//
-//    {
-//      val a, b, c = Replica(MiniSocial())
-//
-//      a.mod(_.setMessage("Welcome To DARE"))
-//      b.mod(_.like())
-//      c.mod(_.like())
-//      Replica.quiescence(a, b, c)
-//
-//      a.show()
-//      b.show()
-//      c.show()
-//    }
+    val a, b, c = Replica(MiniSocial())
 
-    {
-      val a, b, c = Replica(MiniSocial())
+    a.mod(_.setMessage("Welcome To DARE"))
+    b.mod(_.like())
+    c.mod(_.like())
+    Replica.quiescence(a, b, c)
 
-      a.mod(_.setMessage("Welcome To DARE"))
-      b.mod(_.like())
-      c.mod(_.like())
-      Replica.quiescence(a, b, c)
-
-      a.show()
-      b.show()
-      c.show()
-    }
+    a.show()
+    b.show()
+    c.show()
 
   }
 
