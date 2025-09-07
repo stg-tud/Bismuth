9912e7a9c54f6aaaa33716c09a72a9dc5c799fd2
Hamza <hamza.wardi@stud.tu-darmstadt.de>
2025-09-07 22:57:29 +0200

    delegate some methods to CodingWindow

--- Modules/BFT/src/main/scala/riblt/CodingWindow.scala
+++ Modules/BFT/src/main/scala/riblt/CodingWindow.scala
@@ -1,14 +1,18 @@
 package riblt
 
+import riblt.Operation.{Add, Remove}
+
 import scala.collection.mutable
 
+given ord: Ordering[SymbolMapping] = Ordering.by(_.codedIndex)
+
 class CodingWindow[T](
-    var symbols: List[HashedSymbol[T]],
-    var mappings: List[Mapping],
+    var symbols: List[HashedSymbol[T]] = List.empty[HashedSymbol[T]],
+    var mappings: List[Mapping] = List.empty[Mapping],
     var queue: mutable.PriorityQueue[SymbolMapping] = mutable.PriorityQueue()(using ord.reverse),
     var nextIndex: Int = 0
 ):
-  
+
   def addSymbol(symbol: T)(using Hashable[T]): Unit =
     addHashedSymbol(HashedSymbol(symbol, symbol.hash))
 
@@ -19,3 +23,36 @@ class CodingWindow[T](
     symbols = symbols :+ hashedSymbol
     mappings = mappings :+ mapping
     queue.enqueue(SymbolMapping(symbols.length - 1, mapping.lastIndex.toInt))
+
+  def produceNextCodedSymbol(using Xorable[T])(using T): CodedSymbol[T] =
+    assert(queue.nonEmpty, "you have to add source symbols first")
+    applyCodedSymbol(CodedSymbol.identity, Add)
+
+
+  def applyCodedSymbol(codedSymbol: CodedSymbol[T], op: Operation)(using Xorable[T]): CodedSymbol[T] =
+    if queue.isEmpty then {
+      nextIndex += 1
+      codedSymbol
+    } else {
+      var tmp = codedSymbol
+
+      val list = queue.toList
+      for (element <- list) {
+        if (element.codedIndex == nextIndex) {
+          tmp = op match {
+            case Add => tmp.add(symbols(element.sourceIndex))
+            case Remove => tmp.remove(symbols(element.sourceIndex))
+          }
+          element.codedIndex = mappings(element.sourceIndex).nextIndex.toInt
+        }
+      }
+
+      nextIndex = nextIndex + 1
+      tmp
+    }
+
+
+
+enum Operation:
+ case Add
+ case Remove
