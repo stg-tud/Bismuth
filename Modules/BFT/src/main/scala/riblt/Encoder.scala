87456c184b6961203141085afcfab9dccbbe8334
Hamza <hamza.wardi@stud.tu-darmstadt.de>
2025-09-07 22:56:42 +0200

    refactor encoder

--- Modules/BFT/src/main/scala/riblt/Encoder.scala
+++ Modules/BFT/src/main/scala/riblt/Encoder.scala
@@ -1,38 +1,11 @@
 package riblt
 
-import scala.collection.mutable
-import scala.collection.mutable.PriorityQueue
-
-given ord: Ordering[SymbolMapping] = Ordering.by(_.codedIndex)
-
 class Encoder[T](
-    var symbols: List[HashedSymbol[T]],
-    var mappings: List[Mapping],
-    var queue: mutable.PriorityQueue[SymbolMapping] = mutable.PriorityQueue()(using ord.reverse),
-    var nextIndex: Int = 0
+    var window: CodingWindow[T] = CodingWindow[T]()
 ):
 
   def addSymbol(symbol: T)(using Hashable[T]): Unit =
-    addHashedSymbol(HashedSymbol(symbol, symbol.hash))
+    window.addSymbol(symbol)
 
-  def addHashedSymbol(hashedSymbol: HashedSymbol[T]): Unit =
-    addHashedSymbolWithMapping(hashedSymbol, new Mapping(hashedSymbol.hash))
-
-  def addHashedSymbolWithMapping(hashedSymbol: HashedSymbol[T], mapping: Mapping): Unit =
-    symbols = symbols :+ hashedSymbol
-    mappings = mappings :+ mapping
-    queue.enqueue(SymbolMapping(symbols.length - 1, mapping.lastIndex.toInt))
-
-  def produceNextCodedSymbol()(using Xorable[T])(using T): CodedSymbol[T] =
-    assert(queue.nonEmpty, "you have to add source symbols first")
-
-    var codedSymbol = CodedSymbol.identity
-
-    queue.foreach(element => {
-      if element.codedIndex == nextIndex then
-        codedSymbol = codedSymbol.add(symbols(element.sourceIndex))
-        element.codedIndex = mappings(element.sourceIndex).nextIndex.toInt
-    })
-
-    nextIndex = nextIndex + 1
-    codedSymbol
+  def produceNextCodedSymbol(using t: T)(using Xorable[T]): CodedSymbol[T] =
+    window.produceNextCodedSymbol
