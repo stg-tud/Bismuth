0069cb586b8a16cb97da0cfeb44a3b4a67c9ed36
jonas <jonas@rehnonline.de>
2025-09-28 15:02:54 +0200

    WIP DARE25 project: Proof of Work consensus as Lattice marge

--- Modules/Examples JVM/src/main/scala/ex2025blockchain/Blockchain.scala
+++ Modules/Examples JVM/src/main/scala/ex2025blockchain/Blockchain.scala
@@ -2,31 +2,133 @@ package ex2025blockchain
 
 import ex2025blockchain.Block
 import rdts.base.{Bottom, Lattice}
+import rdts.time.CausalTime
 
+import scala.annotation.tailrec
 
-/**
- * @param inner  a map where the key is the hash value of the block
- * @param latest the hash of the latest block
- * @tparam H The type of the hash
- * @tparam T The type of the data in the block
- */
-case class Blockchain[H, T](inner: Map[H, Block[H, T]], latest: H) {
+/** The implementation ignores the cryptographic concepts used in blockchains and focuses instead on the replication of the blocks
+  *
+  * @param inner  a map where the key is the hash value of the block
+  * @param heads  a set of hashes that do not have a successor, one of the hashes is the head of the valid chain
+  * @tparam H The type of the hash
+  * @tparam T The type of the data in the block
+  */
+case class Blockchain[H, T](inner: Map[H, Block[H, T]], heads: Set[H]) {
 
   type delta = Blockchain[H, T]
 
-  def addBlock(hash: H, data: T): Blockchain[H, T] = {
-    val newBlock = Block(hash, latest, data)
-    Blockchain(Map(hash -> newBlock), hash)
+  /** the head of the longest chain should be the validHead
+    * in case two chains have the same length, the head with the latest timestamp should be the validHead
+    */
+  given headOrdering: Ordering[H] = Ordering.by[H, (Int, CausalTime)] { hash =>
+    (chainLength(hash), inner(hash).timestamp)
+  }
+
+  def validHead: H = heads.max
+
+  /** adds a new block to the blockchain by appending it to the end of the chain
+    *
+    * @param newBlock the block to add
+    * @return the resulting blockchain
+    */
+  def addBlock(newBlock: Block[H, T]): Blockchain[H, T] = {
+    var newHeads = heads - newBlock.previousHash.get
+    newHeads += newBlock.hash
+    Blockchain(Map(newBlock.hash -> newBlock), newHeads)
+  }
+
+  /** adds a new block to the blockchain by appending it to the end of the chain
+    *
+    * @param hash the hash value of the block
+    * @param data the data in the block
+    * @return the resulting blockchain
+    */
+  def addBlock(hash: H, data: T): Blockchain[H, T] = addBlock(Block(hash, validHead, data))
+
+  @tailrec
+  private def chainLength(start: H, length: Int = 0): Int = inner(start).previousHash match {
+    case Some(next) => chainLength(next, length + 1)
+    case None => length + 1
+  }
+
+  def validChainLength: Int = chainLength(validHead)
+
+  /** verify that a given block is part of the current valid blockchain
+    *
+    * @param block the block to validate
+    * @return true if the block is part of the valid blockchain
+    */
+  def verify(block: Block[H, T]): Boolean = {
+    var currentBlock = inner.get(validHead)
+    while currentBlock.isDefined do {
+      if currentBlock.get == block then return true
+      currentBlock = currentBlock.get.previousHash match {
+        case Some(value) => inner.get(value)
+        case None        => None
+      }
+    }
+    false
+  }
+
+  def contains(block: Block[H, T]): Boolean = inner.contains(block.hash)
+
+  def toTreeString: String = {
+    @tailrec
+    def getBranchString(h: H, list: List[String] = List.empty): List[String] = {
+      val block = inner(h)
+      block.previousHash match {
+        case Some(value) => getBranchString(value, list :+ block.data.toString)
+        case None        => list :+ block.data.toString
+      }
+    }
+
+    val branches = heads.map { head => getBranchString(head) }.toList
+    branches.sortBy(_.length).map(_.reverse.map(a => f"($a)").mkString(" <- ")).mkString("\n")
   }
 
 }
 
 object Blockchain {
 
-  def empty[H: Bottom, T: Bottom]: Blockchain[H, T] =
-    Blockchain(Map(Block.genesis[H, T].hash -> Block.genesis), Block.genesis[H, T].hash)
+  def apply[H, T](genesisBlock: Block[H, T]): Blockchain[H, T] =
+    Blockchain(Map(genesisBlock.hash -> genesisBlock), Set(genesisBlock.hash))
 
-  given [H: Bottom, T: Bottom]: Bottom[Blockchain[H, T]] = Bottom.derived
+  given [H: Bottom, T]: Bottom[Blockchain[H, T]] = Bottom.derived
 
-  given [H: Lattice, T: Lattice]: Lattice[Blockchain[H, T]] = Lattice.derived
+  given [H: Lattice, T]: Lattice[Blockchain[H, T]] = (a: Blockchain[H, T], b: Blockchain[H, T]) => {
+
+    /** the merge function keeps orphan branches since one orphan branch can still grow and outgrow the current valid chain
+      *
+      * if both chains have the same length, there is no deterministic behavior for selecting the valid chain
+      * this implementation relies on the latest block to select a winner
+      */
+
+    val newInner = a.inner ++ b.inner
+
+    def getNewChainLength(start: H): Int = newInner.get(start) match {
+      case Some(startBlock) => startBlock.previousHash match {
+          case Some(value) => getNewChainLength(value) + 1
+          case None        => 1
+        }
+      case None => 0
+    }
+
+    @tailrec
+    def blockIsInChain(blockHash: H, chainHead: H): Boolean = {
+      val headBlock = newInner(chainHead)
+      if headBlock.hash == blockHash then return true
+      headBlock.previousHash match {
+        case Some(value) => blockIsInChain(blockHash, value)
+        case None        => false
+      }
+    }
+
+    def subtractHeads(a: Set[H], b: Set[H]): Set[H] = {
+      a.filterNot(ahead => b.filter(bhead => ahead != bhead).exists(bhead => blockIsInChain(ahead, bhead)))
+    }
+
+    val newHeads = subtractHeads(a.heads, b.heads) ++ subtractHeads(b.heads, a.heads)
+
+    Blockchain(newInner, newHeads)
+  }
 }
--- Modules/Examples JVM/src/test/scala/ex2025blockchain/BlockchainTest.scala
+++ Modules/Examples JVM/src/test/scala/ex2025blockchain/BlockchainTest.scala
@@ -6,53 +6,65 @@ import rdts.base.{Bottom, Lattice}
 
 class BlockchainTest extends FunSuite {
   test("simple chain") {
-    given Lattice[String] = (a, b) => {
-      if a.compareTo(b) >= 0 then a else b
-    }
-
+    given Lattice[String] = Lattice.fromOrdering
     given Bottom[String] = Bottom.provide("")
 
-    val a, b, c = Replica(Blockchain.empty[String, String])
+    val a, b, c = Replica(Blockchain(Block("", None, "")))
 
     a.mod(_.addBlock("a", "Welcome To DARE"))
     b.mod(_.addBlock("b", "Hello World"))
     c.mod(_.addBlock("c", "Goodbye World"))
     Replica.quiescence(a, b, c)
 
-    assertEquals(a.buffer.result.state.latest, b.buffer.result.state.latest)
-    assertEquals(b.buffer.result.state.latest, c.buffer.result.state.latest)
+    assertEquals(a.buffer.result.state.validHead, b.buffer.result.state.validHead)
+    assertEquals(b.buffer.result.state.validHead, c.buffer.result.state.validHead)
   }
 
   test("with orphan branches") {
-    given Lattice[String] = (a, b) => { if a.compareTo(b) >= 0 then a else b }
+    given Lattice[String] = Lattice.fromOrdering
     given Bottom[String] = Bottom.provide("")
 
-    val a, b, c = Replica(Blockchain.empty[String, String])
+    val genesisBlock: Block[String, String] = Block("", None, "")
+    var a, b = Blockchain(genesisBlock)
 
-    a.mod(_.addBlock("a", "Welcome To DARE"))
-    b.mod(_.addBlock("b", "Hello World"))
-    c.mod(_.addBlock("c", "Goodbye World"))
-    Replica.quiescence(a, b, c)
+    val block1 = Block("a", a.validHead, "Welcome To DARE")
+    val amod1 = a.addBlock(block1)
+    val block2 = Block("b", b.validHead, "Hello World")
+    val bmod1 = b.addBlock(block2)
+    a = a `merge` amod1
+    a = a `merge` bmod1
+    b = b `merge` bmod1
+    b = b `merge` amod1
 
-    assertEquals(a.buffer.result.state.latest, b.buffer.result.state.latest)
-    assertEquals(b.buffer.result.state.latest, c.buffer.result.state.latest)
+    assertEquals(a.validHead, b.validHead)
+    assert(a.contains(block1))      // block2 is more recent than block1, hence the chain of b is chosen as the valid branch
+    assert(a.verify(block2))
+    assert(b.contains(block1))
+    assert(b.verify(block2))
 
-    b.mod(_.addBlock("e", "Hello again"))
-    b.mod(_.addBlock("f", "Hello again again"))
-    c.mod(_.addBlock("d", "Goodbye again"))
-    Replica.quiescence(b, c)
+    val block3 = Block("e", b.validHead, "Hello again")
+    val bmod2 = b.addBlock(block3)
+    b = b `merge` bmod2
+    val block4 = Block("f", b.validHead, "Hello again again")
+    val bmod3 = b.addBlock(block4)
+    b = b `merge` bmod3
 
-    assertNotEquals(a.buffer.result.state.latest, b.buffer.result.state.latest)
-    assertEquals(b.buffer.result.state.latest, c.buffer.result.state.latest)
+    assertNotEquals(a.validHead, b.validHead)
 
-    val latestBlock = b.buffer.result.state.latest
+    val latestB = b.validHead
 
-    a.mod(_.addBlock("g", "Welcome again"))
-    Replica.quiescence(a, b, c)
+    val block5 = Block("g", a.validHead, "Welcome again")
+    val amod2 = a.addBlock(block5)
+    a = a `merge` amod2
+    a = a `merge` bmod2
+    a = a `merge` bmod3
+    b = b `merge` amod2
 
-    // TODO change to assertEquals once a correct Lattice merge function is implemented
-    assertNotEquals(a.buffer.result.state.latest, latestBlock)
-    assertNotEquals(b.buffer.result.state.latest, latestBlock)
-    assertNotEquals(c.buffer.result.state.latest, latestBlock)
+    assertEquals(a.validHead, latestB)
+    assertEquals(b.validHead, latestB)
+  }
+
+  test("orphan outgrows valid branch") {
+
   }
 }
