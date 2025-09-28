a449f0eb57ff2aec1f0493a9bd8b5ceed1a60606
Bennet Bo Fenner <bennet@zed.dev>
2025-09-28 19:43:00 +0200

    Experiment with replica container that maintains undo/redo stack

--- Modules/RDTs/src/main/scala/rdts/experiments/UndoRedoDeltaBasedMVR.scala
+++ Modules/RDTs/src/main/scala/rdts/experiments/UndoRedoDeltaBasedMVR.scala
@@ -8,6 +8,81 @@ import rdts.datatypes.ReplicatedSet
 import rdts.datatypes.ReplicatedList
 
 object UndoRedoDeltaBased {
+  case class Replica[T](
+      val id: Uid,
+      var state: MVR[T],
+      private var undoStack: List[Operation[T]],
+      private var redoStack: List[Operation[T]],
+  ) {
+    def set(value: T): MVR[T] = {
+      val delta   = state.set(value)(using LocalUid(id))
+      val (_, op) = delta.operations.head
+      undoStack = op :: undoStack
+      redoStack = List.empty
+      apply(delta)
+    }
+
+    def delete(): MVR[T] = {
+      val delta   = state.delete()(using LocalUid(id))
+      val (_, op) = delta.operations.head
+      undoStack = op :: undoStack
+      redoStack = List.empty
+      apply(delta)
+    }
+
+    def undo(): MVR[T] = {
+      if undoStack.isEmpty then return MVR.empty[T]
+
+      val lastOp  = undoStack.head
+      val delta   = state.restore(lastOp.id)(using LocalUid(id))
+      val (_, op) = delta.operations.head
+      undoStack = undoStack.tail
+      redoStack = op :: redoStack
+      apply(delta)
+    }
+
+    def redo(): MVR[T] = {
+      if redoStack.isEmpty then return MVR.empty[T]
+
+      val lastOp = redoStack.head
+
+      val lastAnchor = lastOp.ty match
+        case OperationType.restore(anchor) => anchor
+        case _ => throw new Exception(s"Redo stack contains non restore operation ${lastOp.id}")
+
+      val delta = state.restore(lastOp.id)(using LocalUid(id))
+      undoStack = state.operations(lastAnchor) :: undoStack
+      redoStack = redoStack.tail
+      apply(delta)
+    }
+
+    def receive(other: MVR[T]): Replica[T] = {
+      apply(other)
+      this
+    }
+
+    def undoValues: List[T] = {
+      undoStack
+        .flatMap(_.ty.getValue)
+        .reverse
+    }
+
+    def redoAnchors: List[Id] = {
+      redoStack
+        .flatMap(_.ty.getAnchor)
+        .reverse
+    }
+
+    private def apply(delta: MVR[T]): MVR[T] = {
+      state = state `merge` delta
+      this.state
+    }
+  }
+
+  object Replica {
+    def empty[T](using LocalUid): Replica[T] = Replica(LocalUid.replicaId, MVR.empty, List.empty, List.empty)
+  }
+
   type Id = Dot
 
   enum OperationType[+T]:
@@ -33,11 +108,8 @@ object UndoRedoDeltaBased {
   case class Operation[T](id: Id, predecessors: Set[Dot], ty: OperationType[T])
 
   case class MVR[T](
-      dot: Dot,
       operations: Map[Id, Operation[T]],
       headIds: ReplicatedSet[Id],
-      undoStack: ReplicatedList[Operation[T]],
-      redoStack: ReplicatedList[Operation[T]],
   ) {
     type Delta = MVR[T]
 
@@ -87,101 +159,53 @@ object UndoRedoDeltaBased {
       }
     }
 
-    def set(value: T): Delta = {
+    def set(value: T)(using LocalUid): Delta = {
       applyLocalOperation(OperationType.set(value))
     }
 
-    def delete(): Delta = {
+    def delete()(using LocalUid): Delta = {
       applyLocalOperation(OperationType.delete)
     }
 
-    def undo(): Delta = {
-      given LocalUid = LocalUid(dot.place)
+    def restore(node: Id)(using LocalUid): Delta = {
+      if !operations.contains(node) then
+        throw new Exception(s"Cannot restore non existing operation $node")
 
-      if undoStack.isEmpty then return MVR.forReplica(dot.place)
-
-      val lastOp = undoStack.read(0).get
-      val opId   = dot
-      val delta  = applyLocalOperation(OperationType.restore(lastOp.id))
-      delta.copy(
-        undoStack = undoStack.delete(0),
-        redoStack = redoStack.prepend(delta.operations(opId)),
-      )
+      applyLocalOperation(OperationType.restore(node))
     }
 
-    def redo(): Delta = {
-      given LocalUid = LocalUid(dot.place)
-
-      if redoStack.isEmpty then return return MVR.forReplica(dot.place)
-
-      val lastOp = redoStack.read(0).get
-
-      val lastAnchor = lastOp.ty match
-        case OperationType.restore(anchor) => anchor
-        case _ => throw new Exception(s"Redo stack contains non restore operation ${lastOp.id}")
-
-      applyLocalOperation(OperationType.restore(lastOp.id))
-        .copy(
-          undoStack = undoStack.prepend(operations(lastAnchor)),
-          redoStack = redoStack.delete(0),
-        )
-    }
-
-    private def applyLocalOperation(operationType: OperationType[T]): Delta =
-      given LocalUid = LocalUid(dot.place)
+    private def applyLocalOperation(operationType: OperationType[T])(using LocalUid): Delta =
+      val dots = Dots.from(operations.keys)
+      val dot  = dots.nextDot
 
       if operations.contains(dot) then return this
 
       val operation = Operation(dot, headIds.elements, operationType)
 
-      val newOperations = operations + (operation.id -> operation)
+      val newOperations = Map((operation.id -> operation))
 
       val newRemovedHeadIds = headIds `merge` headIds.removeAll(operation.predecessors)
       val newHeadIds        = newRemovedHeadIds `merge` newRemovedHeadIds.add(operation.id)
 
-      val delta = this.copy(
-        dot = dot.advance,
+      this.copy(
         operations = newOperations,
         headIds = newHeadIds,
       )
-      if operationType.isTerminal then {
-        delta.copy(
-          undoStack = undoStack.prepend(operation),
-          redoStack = redoStack.clear(),
-        )
-      } else {
-        delta
-      }
   }
 
   object MVR {
-    def forReplica[T](id: Uid): MVR[T] = MVR(
-      dot = Dot(id, 0),
+    def empty[T]: MVR[T] = MVR(
       operations = Map.empty,
       headIds = ReplicatedSet.empty,
-      undoStack = ReplicatedList.empty,
-      redoStack = ReplicatedList.empty,
     )
 
     given lattice[T]: Lattice[MVR[T]] = new Lattice[MVR[T]] {
       def merge(left: MVR[T], right: MVR[T]): MVR[T] = {
-        val dot = if left.dot.place == right.dot.place then
-          Dot(left.dot.place, scala.math.max(left.dot.time, right.dot.time))
-        else left.dot
         val headIds    = left.headIds.merge(right.headIds)
         val operations = left.operations ++ right.operations
-
-        val (undoStack, redoStack) = if left.dot.place == right.dot.place then
-          (left.undoStack `merge` right.undoStack, left.redoStack `merge` right.redoStack)
-        else
-          (left.undoStack, left.redoStack)
-
         MVR(
-          dot,
           operations,
           headIds,
-          undoStack,
-          redoStack
         )
       }
     }
--- Modules/RDTs/src/test/scala/test/rdts/bespoke/UndoRedoDeltaBasedMVRTest.scala
+++ Modules/RDTs/src/test/scala/test/rdts/bespoke/UndoRedoDeltaBasedMVRTest.scala
@@ -8,52 +8,36 @@ import rdts.base.Lattice
 import rdts.base.Lattice.syntax.merge
 import rdts.datatypes.ReplicatedSet
 
-case class MVRTestReplica[A](id: Uid, var state: A) {
-  def apply(delta: A)(using Lattice[A]): MVRTestReplica[A] =
-    state = state `merge` delta
-    this
-
-  def mod(f: A => A)(using Lattice[A]) = {
-    apply(f(state))
-  }
-
-  def receive(other: MVRTestReplica[A])(using Lattice[A]) = {
-    apply(other.state)
-  }
-}
-
-object MVRTestReplica {
-  def create[A](n: Int): Array[MVRTestReplica[UndoRedoDeltaBased.MVR[A]]] = {
-    (1 to n).map(i => Uid.predefined(s"R$i")).map(rid =>
-      MVRTestReplica(rid, UndoRedoDeltaBased.MVR.forReplica[A](rid))
-    ).toArray
-  }
+def createTestReplicas[A](n: Int): Array[UndoRedoDeltaBased.Replica[A]] = {
+  (1 to n).map(i => Uid.predefined(s"R$i")).map(uid =>
+    UndoRedoDeltaBased.Replica.empty(using LocalUid(uid))
+  ).toArray
 }
 
 class UndoRedoDeltaBasedMVRTest extends munit.FunSuite {
   test("single set operation") {
-    val Array(replica) = MVRTestReplica.create[Int](1)
-    replica.mod(_.set(42))
+    val Array(replica) = createTestReplicas[Int](1)
+    replica.set(42)
     assertEquals(replica.state.values(), List(42))
   }
 
   test("sequential set operations same replica") {
-    val Array(replica) = MVRTestReplica.create[Int](1)
+    val Array(replica) = createTestReplicas[Int](1)
 
-    replica.mod(_.set(1))
+    replica.set(1)
     assertEquals(replica.state.heads(), List(Dot(replica.id, 0)))
 
-    replica.mod(_.set(2))
+    replica.set(2)
     assertEquals(replica.state.heads(), List(Dot(replica.id, 1)))
     assertEquals(replica.state.values(), List(2))
   }
 
   test("concurrent set operations") {
-    val Array(replica1, replica2) = MVRTestReplica.create[Int](2)
+    val Array(replica1, replica2) = createTestReplicas[Int](2)
 
     // Concurrent sets on different replicas
-    val delta1 = replica1.mod(_.set(1))
-    val delta2 = replica2.mod(_.set(2))
+    val delta1 = replica1.set(1)
+    val delta2 = replica2.set(2)
 
     replica2.receive(delta1)
     replica1.receive(delta2)
@@ -64,11 +48,11 @@ class UndoRedoDeltaBasedMVRTest extends munit.FunSuite {
   }
 
   test("three way concurrent sets") {
-    val Array(replica1, replica2, replica3) = MVRTestReplica.create[Int](3)
+    val Array(replica1, replica2, replica3) = createTestReplicas[Int](3)
 
-    val delta1 = replica1.mod(_.set(1))
-    val delta2 = replica2.mod(_.set(2))
-    val delta3 = replica3.mod(_.set(3))
+    val delta1 = replica1.set(1)
+    val delta2 = replica2.set(2)
+    val delta3 = replica3.set(3)
 
     replica1.receive(delta2).receive(delta3)
     replica2.receive(delta1).receive(delta3)
@@ -80,16 +64,16 @@ class UndoRedoDeltaBasedMVRTest extends munit.FunSuite {
   }
 
   test("causal dependency resolution") {
-    val Array(replica1, replica2) = MVRTestReplica.create[Int](2)
+    val Array(replica1, replica2) = createTestReplicas[Int](2)
 
     // Sequential: op1 → op2
-    val delta1 = replica1.mod(_.set(1))
+    val delta1 = replica1.set(1)
     replica2.receive(delta1)
 
     assertEquals(replica1.state.values(), List(1))
     assertEquals(replica2.state.values(), List(1))
 
-    val delta2    = replica2.mod(_.set(2))
+    val delta2    = replica2.set(2)
     val replica1b = replica1.receive(delta2)
 
     assertEquals(replica1.state.values(), List(2))
@@ -97,22 +81,22 @@ class UndoRedoDeltaBasedMVRTest extends munit.FunSuite {
   }
 
   test("delete operation") {
-    val Array(replica) = MVRTestReplica.create[Int](1)
+    val Array(replica) = createTestReplicas[Int](1)
 
-    replica.mod(_.set(42))
-    replica.mod(_.delete())
+    replica.set(42)
+    replica.delete()
 
     assertEquals(replica.state.values(), List())
   }
 
   test("delete with concurrent set") {
-    val Array(replica1, replica2) = MVRTestReplica.create[Int](2)
+    val Array(replica1, replica2) = createTestReplicas[Int](2)
 
-    val delta1 = replica1.mod(_.set(1))
+    val delta1 = replica1.set(1)
     replica2.receive(delta1)
 
-    val delta2a = replica1.mod(_.delete())
-    val delta2b = replica2.mod(_.set(2))
+    val delta2a = replica1.delete()
+    val delta2b = replica2.set(2)
 
     replica1.receive(delta2b)
     replica2.receive(delta2a)
@@ -122,13 +106,13 @@ class UndoRedoDeltaBasedMVRTest extends munit.FunSuite {
   }
 
   test("concurrent deletes") {
-    val Array(replica1, replica2) = MVRTestReplica.create[Int](2)
+    val Array(replica1, replica2) = createTestReplicas[Int](2)
 
-    val deltaInit = replica1.mod(_.set(1))
+    val deltaInit = replica1.set(1)
     replica2.receive(deltaInit)
 
-    val deltaDelete1 = replica1.mod(_.delete())
-    val deltaDelete2 = replica2.mod(_.delete())
+    val deltaDelete1 = replica1.delete()
+    val deltaDelete2 = replica2.delete()
     replica1.receive(deltaDelete2)
     replica2.receive(deltaDelete1)
 
@@ -137,9 +121,9 @@ class UndoRedoDeltaBasedMVRTest extends munit.FunSuite {
   }
 
   test("apply same operation twice") {
-    val Array(replica) = MVRTestReplica.create[Int](1)
+    val Array(replica) = createTestReplicas[Int](1)
 
-    val delta = replica.mod(_.set(42))
+    val delta = replica.set(42)
     // Applying the same delta again should not change the state
     replica.receive(delta)
 
@@ -147,15 +131,15 @@ class UndoRedoDeltaBasedMVRTest extends munit.FunSuite {
   }
 
   test("complex concurrent scenario (delta-based)") {
-    val Array(replica1, replica2, replica3) = MVRTestReplica.create[Int](3)
+    val Array(replica1, replica2, replica3) = createTestReplicas[Int](3)
 
-    val deltaInit = replica1.mod(_.set(0))
+    val deltaInit = replica1.set(0)
     replica2.receive(deltaInit)
     replica3.receive(deltaInit)
 
-    val delta1a = replica1.mod(_.set(1))
-    val delta1b = replica2.mod(_.delete())
-    val delta1c = replica3.mod(_.set(3))
+    val delta1a = replica1.set(1)
+    val delta1b = replica2.delete()
+    val delta1c = replica3.set(3)
 
     replica1.receive(delta1b).receive(delta1c)
     replica2.receive(delta1a).receive(delta1c)
@@ -167,11 +151,11 @@ class UndoRedoDeltaBasedMVRTest extends munit.FunSuite {
   }
 
   test("empty register operations") {
-    val Array(replica1, replica2) = MVRTestReplica.create[Int](2)
+    val Array(replica1, replica2) = createTestReplicas[Int](2)
 
     assert(replica1.state.values().isEmpty)
 
-    val delta = replica1.mod(_.delete())
+    val delta = replica1.delete()
     replica2.receive(delta)
 
     assert(replica1.state.values().isEmpty)
@@ -179,22 +163,22 @@ class UndoRedoDeltaBasedMVRTest extends munit.FunSuite {
   }
 
   test("heads tracking") {
-    val Array(replica1, replica2) = MVRTestReplica.create[Int](2)
+    val Array(replica1, replica2) = createTestReplicas[Int](2)
 
-    val delta1 = replica1.mod(_.set(1))
+    val delta1 = replica1.set(1)
     replica2.receive(delta1)
 
     assertEquals(replica1.state.heads(), List(Dot(replica1.id, 0)))
     assertEquals(replica2.state.heads(), List(Dot(replica1.id, 0)))
 
-    val delta2 = replica2.mod(_.set(1))
+    val delta2 = replica2.set(1)
     replica1.receive(delta2)
 
     assertEquals(replica1.state.heads(), List(Dot(replica2.id, 0)))
     assertEquals(replica2.state.heads(), List(Dot(replica2.id, 0)))
 
-    val delta3a = replica1.mod(_.set(4))
-    val delta3b = replica2.mod(_.set(3))
+    val delta3a = replica1.set(4)
+    val delta3b = replica2.set(3)
 
     assertEquals(replica1.state.heads(), List(Dot(replica1.id, 1)))
     assertEquals(replica2.state.heads(), List(Dot(replica2.id, 1)))
@@ -207,7 +191,7 @@ class UndoRedoDeltaBasedMVRTest extends munit.FunSuite {
     assertEquals(replica1.state.heads(), List(Dot(replica1.id, 1), Dot(replica2.id, 1)))
     assertEquals(replica2.state.heads(), List(Dot(replica1.id, 1), Dot(replica2.id, 1)))
 
-    val delta4 = replica2.mod(_.set(5))
+    val delta4 = replica2.set(5)
     replica1.receive(delta4)
 
     assertEquals(replica1.state.heads(), List(Dot(replica2.id, 2)))
@@ -223,60 +207,60 @@ class UndoRedoDeltaBasedMVRTest extends munit.FunSuite {
   }
 
   test("simple undo redo") {
-    val Array(replica) = MVRTestReplica.create[Int](1)
+    val Array(replica) = createTestReplicas[Int](1)
 
-    replica.mod(_.set(1))
-    replica.mod(_.delete())
+    replica.set(1)
+    replica.delete()
 
     assertEquals(replica.state.values(), List())
 
-    replica.mod(_.undo())
+    replica.undo()
     assertEquals(replica.state.values(), List(1))
 
-    replica.mod(_.redo())
+    replica.redo()
     assertEquals(replica.state.values(), List())
   }
 
   // This test replicates the example from Figure 2 in the paper
   test("undo/redo paper example") {
-    val Array(replicaA, replicaB) = MVRTestReplica.create[Int](2)
+    val Array(replicaA, replicaB) = createTestReplicas[Int](2)
 
     // op_1
-    val delta1 = replicaA.mod(_.set(1))
+    val delta1 = replicaA.set(1)
     replicaB.receive(delta1)
 
     // op_2
-    val delta2 = replicaB.mod(_.set(2))
+    val delta2 = replicaB.set(2)
     replicaA.receive(delta2)
 
     // op_3_a and op_3_b
-    val delta3a = replicaA.mod(_.set(4))
-    val delta3b = replicaB.mod(_.set(3))
+    val delta3a = replicaA.set(4)
+    val delta3b = replicaB.set(3)
     replicaA.receive(delta3b)
     replicaB.receive(delta3a)
 
     // op_4
-    val delta4 = replicaB.mod(_.set(5))
+    val delta4 = replicaB.set(5)
     replicaA.receive(delta4)
 
     // (1)
-    assertEquals(undoValues(replicaA.state), List(1, 4))
-    assertEquals(redoAnchors(replicaA.state), List())
+    assertEquals(replicaA.undoValues, List(1, 4))
+    assertEquals(replicaA.redoAnchors, List())
     assertEquals(replicaA.state.values(), List(5))
-    assertEquals(undoValues(replicaB.state), List(2, 3, 5))
-    assertEquals(redoAnchors(replicaB.state), List())
+    assertEquals(replicaB.undoValues, List(2, 3, 5))
+    assertEquals(replicaB.redoAnchors, List())
     assertEquals(replicaB.state.values(), List(5))
 
     // op_5_a and op_5_b (undo)
-    val delta5a = replicaA.mod(_.undo())
-    val delta5b = replicaB.mod(_.undo())
+    val delta5a = replicaA.undo()
+    val delta5b = replicaB.undo()
 
     // (2a)
-    assertEquals(undoValues(replicaA.state), List(1))
-    assertEquals(redoAnchors(replicaA.state), List(Dot(replicaA.id, 1)))
+    assertEquals(replicaA.undoValues, List(1))
+    assertEquals(replicaA.redoAnchors, List(Dot(replicaA.id, 1)))
     assertEquals(replicaA.state.values(), List(2))
-    assertEquals(undoValues(replicaB.state), List(2, 3))
-    assertEquals(redoAnchors(replicaB.state), List(Dot(replicaB.id, 2)))
+    assertEquals(replicaB.undoValues, List(2, 3))
+    assertEquals(replicaB.redoAnchors, List(Dot(replicaB.id, 2)))
     assertEquals(replicaB.state.values(), List(4, 3))
 
     // Exchange undo ops
@@ -284,85 +268,73 @@ class UndoRedoDeltaBasedMVRTest extends munit.FunSuite {
     replicaB.receive(delta5a)
 
     // (2b)
-    assertEquals(undoValues(replicaA.state), List(1))
-    assertEquals(redoAnchors(replicaA.state), List(Dot(replicaA.id, 1)))
+    assertEquals(replicaA.undoValues, List(1))
+    assertEquals(replicaA.redoAnchors, List(Dot(replicaA.id, 1)))
     assertEquals(replicaA.state.values(), List(2, 4, 3))
-    assertEquals(undoValues(replicaB.state), List(2, 3))
-    assertEquals(redoAnchors(replicaB.state), List(Dot(replicaB.id, 2)))
+    assertEquals(replicaB.undoValues, List(2, 3))
+    assertEquals(replicaB.redoAnchors, List(Dot(replicaB.id, 2)))
     assertEquals(replicaB.state.values(), List(2, 4, 3))
 
     // op_6 (undo)
-    val delta6 = replicaB.mod(_.undo())
+    val delta6 = replicaB.undo()
     replicaA.receive(delta6)
 
     // (3)
-    assertEquals(undoValues(replicaA.state), List(1))
-    assertEquals(redoAnchors(replicaA.state), List(Dot(replicaA.id, 1)))
+    assertEquals(replicaA.undoValues, List(1))
+    assertEquals(replicaA.redoAnchors, List(Dot(replicaA.id, 1)))
     assertEquals(replicaA.state.values(), List(2))
-    assertEquals(undoValues(replicaB.state), List(2))
-    assertEquals(redoAnchors(replicaB.state), List(Dot(replicaB.id, 2), Dot(replicaB.id, 1)))
+    assertEquals(replicaB.undoValues, List(2))
+    assertEquals(replicaB.redoAnchors, List(Dot(replicaB.id, 2), Dot(replicaB.id, 1)))
     assertEquals(replicaB.state.values(), List(2))
 
     // op_7_a (set) and op_7_b (undo)
-    val delta7a = replicaA.mod(_.set(6))
-    val delta7b = replicaB.mod(_.undo())
+    val delta7a = replicaA.set(6)
+    val delta7b = replicaB.undo()
     replicaA.receive(delta7b)
     replicaB.receive(delta7a)
 
     // (4)
-    assertEquals(undoValues(replicaA.state), List(1, 6))
-    assertEquals(redoAnchors(replicaA.state), List())
+    assertEquals(replicaA.undoValues, List(1, 6))
+    assertEquals(replicaA.redoAnchors, List())
     assertEquals(replicaA.state.values(), List(1, 6))
-    assertEquals(undoValues(replicaB.state), List())
-    assertEquals(redoAnchors(replicaB.state), List(Dot(replicaB.id, 2), Dot(replicaB.id, 1), Dot(replicaB.id, 0)))
+    assertEquals(replicaB.undoValues, List())
+    assertEquals(replicaB.redoAnchors, List(Dot(replicaB.id, 2), Dot(replicaB.id, 1), Dot(replicaB.id, 0)))
     assertEquals(replicaB.state.values(), List(1, 6))
 
     // op_8 (redo)
-    val delta8 = replicaB.mod(_.redo())
+    val delta8 = replicaB.redo()
     replicaA.receive(delta8)
 
     // (5)
-    assertEquals(undoValues(replicaA.state), List(1, 6))
-    assertEquals(redoAnchors(replicaA.state), List())
+    assertEquals(replicaA.undoValues, List(1, 6))
+    assertEquals(replicaA.redoAnchors, List())
     assertEquals(replicaA.state.values(), List(2))
-    assertEquals(undoValues(replicaB.state), List(2))
-    assertEquals(redoAnchors(replicaB.state), List(Dot(replicaB.id, 2), Dot(replicaB.id, 1)))
+    assertEquals(replicaB.undoValues, List(2))
+    assertEquals(replicaB.redoAnchors, List(Dot(replicaB.id, 2), Dot(replicaB.id, 1)))
     assertEquals(replicaB.state.values(), List(2))
 
     // op_9 (redo)
-    val delta9 = replicaB.mod(_.redo())
+    val delta9 = replicaB.redo()
     replicaA.receive(delta9)
 
     // (6)
-    assertEquals(undoValues(replicaA.state), List(1, 6))
-    assertEquals(redoAnchors(replicaA.state), List())
+    assertEquals(replicaA.undoValues, List(1, 6))
+    assertEquals(replicaA.redoAnchors, List())
     assertEquals(replicaA.state.values(), List(2, 4, 3))
-    assertEquals(undoValues(replicaB.state), List(2, 3))
-    assertEquals(redoAnchors(replicaB.state), List(Dot(replicaB.id, 2)))
+    assertEquals(replicaB.undoValues, List(2, 3))
+    assertEquals(replicaB.redoAnchors, List(Dot(replicaB.id, 2)))
     assertEquals(replicaB.state.values(), List(2, 4, 3))
 
     // op_10 (redo)
-    val delta10 = replicaB.mod(_.redo())
+    val delta10 = replicaB.redo()
     replicaA.receive(delta10)
 
     // (7)
-    assertEquals(undoValues(replicaA.state), List(1, 6))
-    assertEquals(redoAnchors(replicaA.state), List())
+    assertEquals(replicaA.undoValues, List(1, 6))
+    assertEquals(replicaA.redoAnchors, List())
     assertEquals(replicaA.state.values(), List(5))
-    assertEquals(undoValues(replicaB.state), List(2, 3, 5))
-    assertEquals(redoAnchors(replicaB.state), List())
+    assertEquals(replicaB.undoValues, List(2, 3, 5))
+    assertEquals(replicaB.redoAnchors, List())
     assertEquals(replicaB.state.values(), List(5))
   }
-}
-
-def undoValues[T](register: UndoRedoDeltaBased.MVR[T]): List[T] = {
-  register.undoStack.toList
-    .flatMap(_.ty.getValue)
-    .reverse
-}
-
-def redoAnchors[T](register: UndoRedoDeltaBased.MVR[T]): List[Dot] = {
-  register.redoStack.toList
-    .flatMap(_.ty.getAnchor)
-    .reverse
 }
