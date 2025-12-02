77bda1652e8e8b7523d86a54c9f7cf178078423f
David Scandurra <31861387+SplotyCode@users.noreply.github.com>
2025-12-02 06:43:26 +0100

    Add undo stack to tabular

--- Modules/Tabular/lib/src/main/scala/com/daimpl/lib/SpreadsheetDeltaAggregator.scala
+++ Modules/Tabular/lib/src/main/scala/com/daimpl/lib/SpreadsheetDeltaAggregator.scala
@@ -9,6 +9,8 @@ class SpreadsheetDeltaAggregator[S](
 
   private type EditFunction = LocalUid ?=> Spreadsheet[S] => Spreadsheet[S]
 
+  private var undoStack: List[EditFunction] = Nil
+
   def editAndGetDelta(initialDelta: Spreadsheet[S] = Spreadsheet.empty[S])(fn: EditFunction)
       : Spreadsheet[S] = {
     val delta = fn(using replicaId)(spreadsheet)
@@ -26,6 +28,12 @@ class SpreadsheetDeltaAggregator[S](
     this
   }
 
+  def editWithUndo(doEdit: EditFunction, undoEdit: EditFunction): SpreadsheetDeltaAggregator[S] = {
+    editAndGetDelta()(doEdit)
+    undoStack = undoEdit :: undoStack
+    this
+  }
+
   def repeatEdit(times: Int, fn: EditFunction): SpreadsheetDeltaAggregator[S] = {
     (0 until times) foreach { _ => edit(fn) }
     this
@@ -35,6 +43,21 @@ class SpreadsheetDeltaAggregator[S](
     spreadsheet = spreadsheet.merge(delta)
     this
   }
+
+  def undoAndGetDelta(initialDelta: Spreadsheet[S] = Spreadsheet.empty[S]): Option[Spreadsheet[S]] = {
+    undoStack match {
+      case undoFn :: rest =>
+        undoStack = rest
+        val delta = undoFn(using replicaId)(spreadsheet)
+        accumulate(delta)
+        Some(initialDelta.merge(delta))
+      case Nil => None
+    }
+  }
+
+  def undo(): Boolean = undoAndGetDelta().isDefined
+
+  def canUndo: Boolean = undoStack.nonEmpty
 
   def visit(fn: Spreadsheet[S] => Unit): SpreadsheetDeltaAggregator[S] = {
     fn(spreadsheet)
