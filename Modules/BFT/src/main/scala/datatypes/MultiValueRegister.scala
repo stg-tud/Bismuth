26b4d7e6268505116df8a80a07e664e8791ebef1
Wardi, Hamza <hamzawerdi@stud.tu-darmstadt.de>
2025-12-03 15:43:59 +0100

    readd file removed by mistake

---
+++ Modules/BFT/src/main/scala/datatypes/MultiValueRegister.scala
@@ -0,0 +1,23 @@
+package datatypes
+
+import crypto.Ed25519Util
+import dag.HashDAG
+
+case class MultiValueRegister[T](
+    hashDAG: HashDAG[T]
+):
+
+    def write(value: T): MultiValueRegister[T] =
+      MultiValueRegister(hashDAG.addEvent(value))
+
+    def read: Set[Option[T]] =
+        val heads = hashDAG.getCurrentHeads
+        heads.filter(e => !hashDAG.autohrIsByzantine(e.author)).map(event => event.content)
+
+    def merge(other: MultiValueRegister[T]): MultiValueRegister[T] =
+      MultiValueRegister[T](this.hashDAG.merge(other.hashDAG))
+
+object MultiValueRegister:
+    def apply[T](): MultiValueRegister[T] =
+        val keyPair = Ed25519Util.generateNewKeyPair
+        MultiValueRegister(HashDAG(keyPair.getPublic, Some(keyPair.getPrivate)))
