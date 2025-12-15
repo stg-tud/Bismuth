5ed2f9e7b8f9d9323feb09d52b2b4004e25a9126
David Scandurra <31861387+SplotyCode@users.noreply.github.com>
2025-12-15 03:38:37 +0100

    Fix undo of remove when remove was never applied

--- Modules/Tabular/lib/src/main/scala/com/daimpl/lib/UndoRecordingSpreadsheet.scala
+++ Modules/Tabular/lib/src/main/scala/com/daimpl/lib/UndoRecordingSpreadsheet.scala
@@ -21,13 +21,15 @@ class UndoRecordingSpreadsheet[S](
 
   override def removeRow(rowIdx: RowIndex)(using LocalUid): Spreadsheet[S] = {
     val undo = delegate.internal.keepRow(rowIdx)
-    pushUndo { s => s `merge` undo }
+    val id = delegate.getRowId(rowIdx).get
+    pushUndo { s => if (s.listRowIds.contains(id)) s `merge` undo else s}
     delegate.removeRow(rowIdx)
   }
 
   override def removeColumn(colIdx: ColumnIndex)(using LocalUid): Spreadsheet[S] = {
     val undo = delegate.internal.keepColumn(colIdx)
-    pushUndo { s => s `merge` undo }
+    val id = delegate.getColId(colIdx).get
+    pushUndo { s => if (s.listColumnIds.contains(id)) s `merge` undo else s}
     delegate.removeColumn(colIdx)
   }
 
