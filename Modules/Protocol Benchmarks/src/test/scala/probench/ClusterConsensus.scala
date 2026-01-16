b11c9dcc3b00046514bccade12a613377c56f63c
Julian <git@julian-haas.de>
2026-01-16 15:15:25 +0100

    more cluster consensus tests

--- Modules/Protocol Benchmarks/src/test/scala/probench/ClusterConsensus.scala
+++ Modules/Protocol Benchmarks/src/test/scala/probench/ClusterConsensus.scala
@@ -99,4 +99,88 @@ class ClusterConsensus extends munit.FunSuite {
     assertEquals(nodes(2).cluster.state.log.size, 2)
 
   }
+  test("consensus with one node") {
+
+    given JsonValueCodec[ClusterState] =
+      JsonCodecMaker.make(CodecMakerConfig.withMapAsArray(true))
+    given clusterCodec: JsonValueCodec[ProtocolMessage[ClusterState]] =
+      JsonCodecMaker.make(CodecMakerConfig.withMapAsArray(true))
+    given JsonValueCodec[ClientState] =
+      JsonCodecMaker.make(CodecMakerConfig.withMapAsArray(true))
+    given clientCodec: JsonValueCodec[ProtocolMessage[ClientState]] =
+      JsonCodecMaker.make(CodecMakerConfig.withMapAsArray(true))
+
+    val ids = Set("Node1").map(Uid.predefined)
+    given Participants(ids)
+    val nodes @ primary :: x =
+      ids.map { id => KeyValueReplica(id, ids, offloadSending = false, maxClients = Some(1)) }.toList: @unchecked
+    val connection = channels.SynchronousLocalConnection[ProtocolMessage[ClusterState]]()
+    primary.cluster.dataManager.addObjectConnection(connection.server)
+
+    val clientConnection = channels.SynchronousLocalConnection[ProtocolMessage[ClientState]]()
+
+    primary.client.dataManager.addObjectConnection(clientConnection.server)
+
+    val clientUid = Uid.gen()
+    val client    = ProBenchClient(clientUid, blocking = true, logTimings = false)
+    client.dataManager.addObjectConnection(clientConnection.client(clientUid.toString))
+
+    client.printResults = false
+
+    client.write("test", "Hi")
+    client.read("test")
+
+    assertEquals(primary.client.state.requestsSorted, List.empty)
+    assertEquals(primary.cluster.state.read.length, 2)
+
+    for n <- Range(0,1000) do {
+      client.write(n.toString, "value")
+    }
+
+    assertEquals(primary.cluster.state.read.length, 1002)
+
+    def investigateUpkeep(state: ClusterState)(using LocalUid) = {
+      val delta  = state.upkeep
+      val merged = state `merge` delta
+      assert(state != merged)
+      assert(delta `inflates` state, delta)
+    }
+
+    def runUpkeep() = while {
+      nodes.filter(_.cluster.needsUpkeep()).exists { n =>
+        investigateUpkeep(n.cluster.state)(using n.localUid)
+        n.cluster.forceUpkeep()
+        true
+      }
+    } do ()
+
+    runUpkeep()
+
+    nodes.foreach(node => assert(!node.cluster.needsUpkeep(), node.uid))
+
+    def noUpkeep(keyValueReplica: KeyValueReplica): Unit = {
+      val current = keyValueReplica.cluster.state
+      assertEquals(
+        current `merge` current.upkeep(using keyValueReplica.localUid),
+        current,
+        s"${keyValueReplica.uid} upkeep"
+      )
+    }
+
+    nodes.foreach(noUpkeep)
+
+    // simulate crash
+
+
+    client.printResults = false
+
+    client.write("test2", "Hi")
+    client.read("test2")
+
+    runUpkeep()
+
+    nodes.foreach(noUpkeep)
+
+
+  }
 }
