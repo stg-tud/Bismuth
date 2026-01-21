9383a5090fa83e349805d33ef7a91241a635c9f9
Christian Kuessner <17302779+ckuessner@users.noreply.github.com>
2026-01-21 22:51:46 +0100

    WIP: add filtered rdt anti entropy

---
+++ Modules/Access Control/src/main/scala/lofi_acl/sync/signed/FilteredRdtAntiEntropy.scala
@@ -0,0 +1,86 @@
+package lofi_acl.sync.signed
+
+import crypto.PublicIdentity
+import crypto.channels.PrivateIdentity
+import lofi_acl.bft.HashDag.Encoder
+import lofi_acl.bft.{Acl, BftDelta, Hash, HashDag}
+import lofi_acl.sync.SynchronizedMutableArrayBufferDeltaStore
+import rdts.base.{Bottom, Decompose, Lattice, Uid}
+import rdts.filters.{Filter, PermissionTree}
+import rdts.time.{Dot, Dots}
+
+import java.util.concurrent.atomic.{AtomicLong, AtomicReference}
+
+class FilteredRdtAntiEntropy[State: {Decompose, Lattice, Bottom, Filter}](
+    private val localIdentity: PrivateIdentity,
+    initialDotValue: Long = 0
+)(
+    using Encoder[SignedDelta[State]]
+) {
+  private def currentAcl: (Set[Hash], Acl)                              = ???
+  private def hashDag: HashDag[BftDelta[Acl], Acl]                      = ???
+  private def requestDeltas(deltas: Dots, remote: PublicIdentity): Unit = ???
+  private def notifyStateChanged(delta: State): Unit                    = ???
+  private def onAclChangeNotification(delta: Acl): Unit                 =
+    // TODO: request all filtered deltas
+    ???
+
+  private val dotCounter = AtomicLong(0)
+  private val localUid   = Uid(localIdentity.getPublic.id)
+
+  private val latestState: AtomicReference[(Dots, State)] = AtomicReference((Dots.empty, Bottom[State].empty))
+  private val deltaStore                          = SynchronizedMutableArrayBufferDeltaStore[SignedDelta[State]]()
+  private val filteredDots: AtomicReference[Dots] = AtomicReference(Dots.empty)
+  private val missingDots: AtomicReference[Dots]  = AtomicReference(Dots.empty)
+
+  def localMutation(delta: State): Unit = {
+    val decomposed = delta.decomposed.map(d =>
+      SignedDelta.fromDelta(localIdentity, Dot(localUid, dotCounter.getAndIncrement()), d)
+    )
+    ???
+  }
+
+  def receiveDeltas(
+      unverifiedDeltas: Seq[SignedDelta[State]],
+      filtered: Dots,
+      remoteAcl: Set[Hash],
+      remote: PublicIdentity
+  ): Unit = {
+    val (localAclHeads, localAcl) = currentAcl
+
+    // Check signatures and enforce ACL
+    val deltas = unverifiedDeltas
+      .filter(_.isSignatureValid)
+      .flatMap(delta =>
+        delta.filtered(localAcl.write.getOrElse(PublicIdentity(delta.dot.place.delegate), PermissionTree.empty))
+      )
+
+    // If remote acl < local acl, remote filter might be too restrictive
+    if !filtered.isEmpty && remoteAcl != localAclHeads then {
+      val localAclDeltas: Set[Hash] = hashDag.heads
+      if remoteAcl.subsetOf(localAclDeltas) // remote acl > local acl
+      then requestDeltas(filtered, remote)  // Note that we already sent missing ACL deltas to remote
+    } else {
+      val newFiltered = filteredDots.updateAndGet(known => known.union(filtered))
+      assert(deltaStore.dots.intersect(newFiltered).isEmpty) // TODO: remove this check at some point
+    }
+
+    var appliedDeltas: Dots     = Dots.empty
+    var accumulatedDelta: State = Bottom[State].empty
+    deltas.foreach { d =>
+      deltaStore.put(d.dot, d)
+      appliedDeltas = appliedDeltas.add(d.dot)
+      accumulatedDelta = accumulatedDelta.merge(d.payload)
+    }
+    if !appliedDeltas.isEmpty then {
+      latestState.updateAndGet((oldDots, oldState) => (oldDots.union(appliedDeltas), oldState.merge(accumulatedDelta)))
+      notifyStateChanged(accumulatedDelta)
+    }
+  }
+
+  def updatePeerDeltaKnowledge(deltas: Dots, remote: PublicIdentity): Unit =
+    ???
+
+  def respondToDeltaRequest(missingRdtDeltas: Dots, remote: PublicIdentity): Unit =
+    ???
+}
