31081316288c4e72110c4d50025d8a7f7d1cbbfe
Christian Kuessner <17302779+ckuessner@users.noreply.github.com>
2026-02-28 19:04:57 +0100

    Fix message handling

--- Modules/Access Control/src/main/scala/lofi_acl/sync/signed/AclEnforcingSync.scala
+++ Modules/Access Control/src/main/scala/lofi_acl/sync/signed/AclEnforcingSync.scala
@@ -10,6 +10,7 @@ import lofi_acl.sync.JsoniterCodecs.syncMsgCodec
 import lofi_acl.sync.signed.AclEnforcingSync.SyncMsg.{MyAclVersionIs, MyPeersAre, MyRdtVersionIs}
 import lofi_acl.sync.signed.AclEnforcingSync.{SyncMsg, encoder}
 import lofi_acl.sync.{ChannelConnectionManager, ConnectionManager, JsoniterCodecs, MessageReceiver}
+import lofi_acl.travelplanner.Debug
 import rdts.base.{Bottom, Decompose, Lattice}
 import rdts.filters.{Filter, PermissionTree}
 import rdts.time.Dots
@@ -19,15 +20,20 @@ import java.util.concurrent.Executors
 class AclEnforcingSync[State: {JsonValueCodec, Bottom, Decompose, Lattice, Filter}](
     localIdentity: PrivateIdentity,
     connectionManagerProvider: (PrivateIdentity, MessageReceiver[MessageBuffer]) => ConnectionManager =
-      (id, receiver) => ChannelConnectionManager(id.tlsKeyPem, id.tlsCertPem, id.getPublic, receiver),
+      (id, receiver) =>
+        ChannelConnectionManager(id.tlsKeyPem, id.tlsCertPem, id.getPublic, receiver, disableLogging = false),
     aclGenesis: BftDelta[Acl],
     onRdtChanged: State => Unit
 ) {
   private val messageHandlerExecutor = Executors.newSingleThreadExecutor() // Executes the message handling logic
   private val connectionManager: ConnectionManager = {
     val msgReceiver = new MessageReceiver[MessageBuffer] {
-      override def receivedMessage(msg: MessageBuffer, fromUser: PublicIdentity): Unit =
-        messageHandlerExecutor.execute(() => handleMessage(readFromArray(msg.asArray), fromUser))
+      override def receivedMessage(msgBuf: MessageBuffer, fromUser: PublicIdentity): Unit =
+        messageHandlerExecutor.execute(() =>
+            val msg: SyncMsg[State] = readFromArray(msgBuf.asArray)
+            Debug.received(msg, fromUser)
+            handleMessage(msg, fromUser)
+        )
 
       override def connectionEstablished(newRemote: PublicIdentity): Unit = {
         messageHandlerExecutor.execute(() =>
@@ -53,13 +59,19 @@ class AclEnforcingSync[State: {JsonValueCodec, Bottom, Decompose, Lattice, Filte
   private def onAclChange(delta: Acl): Unit = if rdtAntiEntropy != null then rdtAntiEntropy.onAclChanged(delta)
 
   def handleMessage(msg: SyncMsg[State], remote: PublicIdentity): Unit = msg match {
-    case SyncMsg.DataDeltas(deltas, filtered, remoteAcl) => aclAntiEntropy.updatePeerAclKnowledge(remoteAcl, remote)
-    case SyncMsg.AclDeltas(deltas)                       => aclAntiEntropy.receiveDeltas(deltas, remote)
-    case SyncMsg.MyPeersAre(peers)                       =>
+    case SyncMsg.DataDeltas(deltas, filtered, remoteAcl) =>
+      aclAntiEntropy.updatePeerAclKnowledge(remoteAcl, remote)
+      rdtAntiEntropy.receiveDeltas(deltas, filtered, remoteAcl, remote)
+    case SyncMsg.AclDeltas(deltas) => aclAntiEntropy.receiveDeltas(deltas, remote)
+    case SyncMsg.MyPeersAre(peers) =>
       val established = connectionManager.connectedPeers
-      peers.filter((id, _) => established(id)).foreach { case (_, (host, port)) =>
-        connectionManager.connectTo(host, port)
-      }
+      println(peers.filterNot((id, _) => established(id)))
+      peers
+        .filterNot((id, _) => established(id) || localIdentity.getPublic == id)
+        .foreach { case (id, (host, port)) =>
+          println(s"Attempting connection to $id")
+          connectionManager.connectTo(host, port)
+        }
     case SyncMsg.MyRdtVersionIs(remoteDataDeltas) => rdtAntiEntropy.updatePeerDeltaKnowledge(remoteDataDeltas, remote)
     case SyncMsg.MyAclVersionIs(remoteAclHeads)   => aclAntiEntropy.updatePeerAclKnowledge(remoteAclHeads, remote)
     case SyncMsg.SendMe(missingRdtDeltas, missingAclDeltas) =>
@@ -69,14 +81,20 @@ class AclEnforcingSync[State: {JsonValueCodec, Bottom, Decompose, Lattice, Filte
 
   def connect(host: String, port: Int): Unit = connectionManager.connectTo(host, port)
 
-  def currentState: State = rdtAntiEntropy.currentState._2
-
   def aclRootOp: BftDelta[Acl] = {
     val hashDag = aclAntiEntropy.currentHashDag
     hashDag.deltas(hashDag.root)
   }
 
+  def stateVersion: Dots = rdtAntiEntropy.currentState._1
+
+  def currentState: State = rdtAntiEntropy.currentState._2
+
+  def aclVersion: Set[Hash] = aclAntiEntropy.currentAcl._1
+
   def currentAcl: Acl = aclAntiEntropy.currentAcl._2
+
+  def connectedPeers: Set[PublicIdentity] = connectionManager.connectedPeers
 
   def delegatePermission(read: Map[PublicIdentity, PermissionTree], write: Map[PublicIdentity, PermissionTree]): Unit =
     aclAntiEntropy.mutate(Acl(read, write, Set.empty, Set.empty))
