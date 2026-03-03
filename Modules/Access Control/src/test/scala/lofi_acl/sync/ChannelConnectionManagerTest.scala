dab6c1e65f320ae792f4fbbdaaa7f75df0d740ab
Christian Kuessner <17302779+ckuessner@users.noreply.github.com>
2026-03-03 13:19:09 +0100

    Add test for three concurrent connections / changed ConnectionManager

--- Modules/Access Control/src/test/scala/lofi_acl/sync/ChannelConnectionManagerTest.scala
+++ Modules/Access Control/src/test/scala/lofi_acl/sync/ChannelConnectionManagerTest.scala
@@ -3,7 +3,7 @@ package lofi_acl.sync
 import channels.{ArrayMessageBuffer, MessageBuffer}
 import crypto.PublicIdentity
 import crypto.channels.IdentityFactory
-import ChannelConnectionManagerTest.{DEBUG, TIMEOUT_MS, buf, unbuf}
+import lofi_acl.sync.ChannelConnectionManagerTest.{DEBUG, TIMEOUT_MS, buf, unbuf}
 import munit.FunSuite
 
 import java.util.concurrent.TimeUnit
@@ -20,7 +20,7 @@ object ChannelConnectionManagerTest {
 class ChannelConnectionManagerTest extends FunSuite {
   private val idA = IdentityFactory.createNewIdentity
   private val idB = IdentityFactory.createNewIdentity
-  // private val idC = IdentityFactory.createNewIdentity
+  private val idC = IdentityFactory.createNewIdentity
   // private val idD = IdentityFactory.createNewIdentity
 
   test("Two replicas") {
@@ -33,21 +33,85 @@ class ChannelConnectionManagerTest extends FunSuite {
     connManA.acceptIncomingConnections()
     connManB.connectTo("localhost", connManA.listenPort.get) // B -> A
 
+    // Duplicate connection attempts (A -> B)
+    connManB.acceptIncomingConnections()
+    connManA.connectTo("localhost", connManB.listenPort.get)
+
     assertEquals(receiverA.connectionQueue.poll(TIMEOUT_MS, TimeUnit.MILLISECONDS), idB.getPublic)
     assertEquals(receiverB.connectionQueue.poll(TIMEOUT_MS, TimeUnit.MILLISECONDS), idA.getPublic)
 
     connManA.send(idB.getPublic, buf("Hello"))
     assertEquals(receiverB.messageQueue.poll(TIMEOUT_MS, TimeUnit.MILLISECONDS).unbuf, ("Hello", idA.getPublic))
 
-    connManB.send(idB.getPublic, buf("World"))
-
-    // Duplicate connections are accepted (A -> B)
-    connManB.acceptIncomingConnections()
-    connManA.connectTo("localhost", connManB.listenPort.get)
-    assertEquals(receiverA.connectionQueue.poll(TIMEOUT_MS, TimeUnit.MILLISECONDS), idB.getPublic)
-    assertEquals(receiverB.connectionQueue.poll(TIMEOUT_MS, TimeUnit.MILLISECONDS), idA.getPublic)
+    connManB.send(idA.getPublic, buf("World"))
+    assertEquals(receiverA.messageQueue.poll(TIMEOUT_MS, TimeUnit.MILLISECONDS).unbuf, ("World", idB.getPublic))
 
     connManA.shutdown()
     connManB.shutdown()
+  }
+
+  test("Three replicas with duplicates") {
+    val receiverA = QueueAppendingMessageReceiver()
+    val receiverB = QueueAppendingMessageReceiver()
+    val receiverC = QueueAppendingMessageReceiver()
+
+    val connManA =
+      ChannelConnectionManager(idA.tlsKeyPem, idA.tlsCertPem, idA.getPublic, receiverA, disableLogging = !DEBUG)
+    val connManB =
+      ChannelConnectionManager(idB.tlsKeyPem, idB.tlsCertPem, idB.getPublic, receiverB, disableLogging = !DEBUG)
+    val connManC =
+      ChannelConnectionManager(idC.tlsKeyPem, idC.tlsCertPem, idC.getPublic, receiverC, disableLogging = !DEBUG)
+
+    connManA.acceptIncomingConnections()
+    connManB.acceptIncomingConnections()
+    connManC.acceptIncomingConnections()
+
+    connManA.connectTo("localhost", connManB.listenPort.get) // A -> B
+    connManB.connectTo("localhost", connManA.listenPort.get) // B -> A
+    connManC.connectTo("localhost", connManA.listenPort.get) // C -> A
+    connManA.connectTo("localhost", connManC.listenPort.get) // A -> C
+    connManB.connectTo("localhost", connManC.listenPort.get) // B -> C
+    connManC.connectTo("localhost", connManB.listenPort.get) // C -> B
+
+    assertEquals(
+      Set(
+        receiverA.connectionQueue.poll(TIMEOUT_MS, TimeUnit.MILLISECONDS),
+        receiverA.connectionQueue.poll(TIMEOUT_MS, TimeUnit.MILLISECONDS)
+      ),
+      Set(idB.getPublic, idC.getPublic)
+    )
+    assertEquals(
+      Set(
+        receiverB.connectionQueue.poll(TIMEOUT_MS, TimeUnit.MILLISECONDS),
+        receiverB.connectionQueue.poll(TIMEOUT_MS, TimeUnit.MILLISECONDS)
+      ),
+      Set(idA.getPublic, idC.getPublic)
+    )
+    assertEquals(
+      Set(
+        receiverC.connectionQueue.poll(TIMEOUT_MS, TimeUnit.MILLISECONDS),
+        receiverC.connectionQueue.poll(TIMEOUT_MS, TimeUnit.MILLISECONDS)
+      ),
+      Set(idA.getPublic, idB.getPublic)
+    )
+
+    connManA.send(idB.getPublic, buf("Hello"))
+    connManA.send(idC.getPublic, buf("Hello"))
+    assertEquals(receiverB.messageQueue.poll(TIMEOUT_MS, TimeUnit.MILLISECONDS).unbuf, ("Hello", idA.getPublic))
+    assertEquals(receiverC.messageQueue.poll(TIMEOUT_MS, TimeUnit.MILLISECONDS).unbuf, ("Hello", idA.getPublic))
+
+    connManB.send(idA.getPublic, buf("World"))
+    connManB.send(idC.getPublic, buf("World"))
+    assertEquals(receiverA.messageQueue.poll(TIMEOUT_MS, TimeUnit.MILLISECONDS).unbuf, ("World", idB.getPublic))
+    assertEquals(receiverC.messageQueue.poll(TIMEOUT_MS, TimeUnit.MILLISECONDS).unbuf, ("World", idB.getPublic))
+
+    connManC.send(idA.getPublic, buf("!"))
+    connManC.send(idB.getPublic, buf("!"))
+    assertEquals(receiverA.messageQueue.poll(TIMEOUT_MS, TimeUnit.MILLISECONDS).unbuf, ("!", idC.getPublic))
+    assertEquals(receiverB.messageQueue.poll(TIMEOUT_MS, TimeUnit.MILLISECONDS).unbuf, ("!", idC.getPublic))
+
+    connManA.shutdown()
+    connManB.shutdown()
+    connManC.shutdown()
   }
 }
