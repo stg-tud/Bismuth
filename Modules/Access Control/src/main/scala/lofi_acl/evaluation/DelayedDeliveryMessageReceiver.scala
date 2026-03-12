e8a54000997dc4297848529bf2d5b404f897cbe8
Christian Kuessner <17302779+ckuessner@users.noreply.github.com>
2026-03-12 17:23:08 +0100

    Use cached thread pool

--- Modules/Access Control/src/main/scala/lofi_acl/evaluation/DelayedDeliveryMessageReceiver.scala
+++ Modules/Access Control/src/main/scala/lofi_acl/evaluation/DelayedDeliveryMessageReceiver.scala
@@ -3,9 +3,14 @@ package lofi_acl.evaluation
 import crypto.PublicIdentity
 import lofi_acl.sync.MessageReceiver
 
+import java.util.concurrent.Executors
+
 object DelayedDeliveryMessageReceiver {
   def delay[MSG](delay: Int, messageReceiver: MessageReceiver[MSG]): MessageReceiver[MSG] = new MessageReceiver[MSG]:
-      override def receivedMessage(msg: MSG, fromUser: PublicIdentity): Unit = Thread.ofVirtual().start { () =>
+      private val threadPool = Executors.newCachedThreadPool()
+      threadPool.execute { () => }
+
+      override def receivedMessage(msg: MSG, fromUser: PublicIdentity): Unit = threadPool.execute { () =>
         Thread.sleep(delay)
         messageReceiver.receivedMessage(msg, fromUser)
       }: Unit
