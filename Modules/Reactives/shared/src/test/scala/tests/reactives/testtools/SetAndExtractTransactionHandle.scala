fa97a0f68684e9b4261d074dc1b076d9bd2ea61a
ragnar <git@rmgk.de>
2026-05-17 00:18:31 +0200

    rename test package

---
+++ Modules/Reactives/shared/src/test/scala/tests/reactives/testtools/SetAndExtractTransactionHandle.scala
@@ -0,0 +1,19 @@
+package tests.reactives.testtools
+
+import reactives.SelectedScheduler.candidate.State
+import reactives.core.{Initializer, Scheduler}
+import reactives.operator.Source
+
+class SetAndExtractTransactionHandle(val api: reactives.default.type) {
+  def SetAndExtractTransactionHandle[A, N](
+      source: Source[A] { type State[V] = reactives.SelectedScheduler.State[V] },
+      value: A
+  )(using
+      engine: Scheduler[State]
+  ): Initializer[State] = {
+    engine.forceNewTransaction(source) { implicit t =>
+      source.admit(value)
+      t.tx.initializer
+    }
+  }
+}
