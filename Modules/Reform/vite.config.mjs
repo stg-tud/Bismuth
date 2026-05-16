7bea047a1cf5f528989d5b311610b573d1f710ea
ragnar <git@rmgk.de>
2026-05-16 14:22:13 +0200

    minor js deps cleanup

--- Modules/Reform/vite.config.mjs
+++ Modules/Reform/vite.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
-import { VitePWA } from 'vite-plugin-pwa';
-import { visualizer } from "rollup-plugin-visualizer";
-import * as path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: 'inline',
+      registerType: "autoUpdate",
+      injectRegister: "inline",
       workbox: {
         maximumFileSizeToCacheInBytes: 10 * 1000 * 1000,
         navigateFallbackDenylist: [
@@ -19,38 +17,35 @@ export default defineConfig({
         clientsClaim: true,
         skipWaiting: true,
       },
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'safari-pinned-tab.svg', 'favicon-32x32.png', 'favicon-16x16.png'],
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "safari-pinned-tab.svg",
+        "favicon-32x32.png",
+        "favicon-16x16.png",
+      ],
       manifest: {
         name: "ReForm",
         short_name: "ReForm",
         icons: [
           { src: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
           { src: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
-          { src: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
+          { src: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png", purpose: "any maskable" },
         ],
         theme_color: "#ffffff",
         background_color: "#ffffff",
-        display: "standalone"
-      }
+        display: "standalone",
+      },
     }),
-    visualizer(),
   ],
-  root: './src/main/js',
+  root: "./src/main/js",
   envDir: process.cwd(),
   build: {
-    outDir: path.join(process.cwd(), "dist"),
+    outDir: "./dist",
     sourcemap: true,
   },
   define: {
     APP_VERSION: JSON.stringify(process.env.npm_package_version),
-  },
-  test: {
-    environment: "jsdom",
-    testTimeout: 300000,
-    hookTimeout: 300000,
-  },
-  preview: {
-    port: 5173,
   },
   clearScreen: false,
 });
