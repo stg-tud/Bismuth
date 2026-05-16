9f59d52bb7b68966dd64e09c331f92a739d94576
ragnar <git@rmgk.de>
2026-05-16 14:24:06 +0200

    remove pwa plugin

--- Modules/Reform/vite.config.mjs
+++ Modules/Reform/vite.config.mjs
@@ -1,43 +1,7 @@
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [
-    VitePWA({
-      registerType: "autoUpdate",
-      injectRegister: "inline",
-      workbox: {
-        maximumFileSizeToCacheInBytes: 10 * 1000 * 1000,
-        navigateFallbackDenylist: [
-          /^\/discovery-server-websocket/,
-          /^\/always-online-peer/,
-          /^\/api/,
-          /.*\.js\.map/,
-        ],
-        clientsClaim: true,
-        skipWaiting: true,
-      },
-      includeAssets: [
-        "favicon.ico",
-        "apple-touch-icon.png",
-        "safari-pinned-tab.svg",
-        "favicon-32x32.png",
-        "favicon-16x16.png",
-      ],
-      manifest: {
-        name: "ReForm",
-        short_name: "ReForm",
-        icons: [
-          { src: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
-          { src: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
-          { src: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png", purpose: "any maskable" },
-        ],
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
-        display: "standalone",
-      },
-    }),
-  ],
+  plugins: [],
   root: "./src/main/js",
   envDir: process.cwd(),
   build: {
