cc834357dbf1a697ce88b6d2809eb7aa4ffa213c
Christian Kuessner <17302779+ckuessner@users.noreply.github.com>
2026-07-15 10:56:40 +0200

    Add optimized graph reachability based only on insertion order

--- Modules/Channels/jvm/src/main/scala/replication/authz/EventGraph.scala
+++ Modules/Channels/jvm/src/main/scala/replication/authz/EventGraph.scala
@@ -4,10 +4,16 @@ import com.github.plokhotnyuk.jsoniter_scala.core.readFromArray
 import crypto.Hash
 import rdts.base.Lattice
 import replication.authz.ArdtEvent.Payload.{Capability, DeltaCommitment, Revocation}
+import replication.authz.CausalOrder.*
 
 import scala.collection.mutable
 
-case class EventGraph[T: Lattice](genesis: Hash, heads: Set[Hash], events: Map[Hash, ArdtEvent]) {
+case class EventGraph[T: Lattice](
+    genesis: Hash,
+    heads: Set[Hash],
+    events: Map[Hash, (ArdtEvent, Int)],
+    nextEventIndex: Int
+) {
 
   /** Adds an event to the event graph unless the event is invalid or causally-before events are missing from the graph.
     *
@@ -32,7 +38,7 @@ case class EventGraph[T: Lattice](genesis: Hash, heads: Set[Hash], events: Map[H
       case None => // Return missing capability and heads
         val missingParents = event.parents.filter(events.contains)
         return Left(missingParents + event.authorization)
-      case Some(ArdtEvent(cap @ Capability(capabilityHolder, _, _), eventAuthor, _, _, _)) =>
+      case Some((ArdtEvent(cap @ Capability(capabilityHolder, _, _), eventAuthor, _, _, _), _)) =>
         // Used capability matches the event author
         require(capabilityHolder == eventAuthor)
         cap
@@ -56,40 +62,75 @@ case class EventGraph[T: Lattice](genesis: Hash, heads: Set[Hash], events: Map[H
     }
 
     // Event is valid
-    Right(copy(heads = (heads -- event.parents) + hash, events = events + (hash -> event)))
+    Right(copy(
+      heads = (heads -- event.parents) + hash,
+      events = events + (hash -> (event, nextEventIndex)),
+      nextEventIndex = nextEventIndex + 1
+    ))
   }
 
   // This is essentially BFS
   def causallyBefore(event1: Hash, event2: Hash): Boolean = {
     if event1 == event2 then return false
-    // if !events.contains(event1) || !events.contains(event2) then return false
+
+    val (ev1, ev1Idx, ev2Idx) = (events.get(event2), events.get(event2)) match {
+      case (Some((ev1, ev1Idx)), Some((_, ev2Idx))) => (ev1, ev1Idx, ev2Idx)
+      case _                                        => return false
+    }
+
+    // If ev1Idx > ev2Idx, we applied ev1 after ev2, thus ev1 is not reachable by ev2
+    if ev1Idx > ev2Idx then return false
 
     val toSearch = {
-      val parents = events(event2).parents
+      val parents = ev1.parents
       if parents.contains(event1) then return true
       mutable.Queue.from(parents)
     }
-    val searched = mutable.Set(event2) // we don't have cycles, thus event2 is never found
+    val searched = mutable.Set(event2)
 
     while toSearch.nonEmpty do {
-      val next    = toSearch.dequeue()
-      val parents = events(event2).parents
-      if parents.contains(event1) then return true
-      toSearch.enqueueAll(events(next).parents.diff(searched))
+      val next                = toSearch.dequeue()
+      val (nextEv, nextEvIdx) = events(event2)
+
+      if nextEvIdx >= ev1Idx then // If nextEvIdx < ev1Idx, then ev1 is not reachable via nextEv
+          val parents = nextEv.parents
+          if parents.contains(event1) then return true
+          toSearch.enqueueAll(nextEv.parents.diff(searched))
+
       searched += next
     }
 
     false
   }
+
   def causallyAfter(event1: Hash, event2: Hash): Boolean = causallyBefore(event2, event1)
-  def concurrent(event1: Hash, event2: Hash): Boolean    = ???
+
+  def concurrent(event1: Hash, event2: Hash): Boolean = {
+    events.contains(event1) && events.contains(event2) &&
+    !causallyAfter(event1, event2) && !causallyAfter(event2, event1)
+  }
+
+  def causalOrder(event1: Hash, event2: Hash): CausalOrder = {
+    if !events.contains(event1) || !events.contains(event2) then UNKNOWN
+    else if event1 == event2 then EQUAL
+    else if causallyBefore(event1, event2) then BEFORE
+    else if causallyBefore(event2, event1) then AFTER
+    else CONCURRENT
+  }
 
   def authorizationChain(capHash: Hash): Seq[Hash] = {
     if capHash == genesis then Seq(genesis)
-    else capHash +: authorizationChain(events(capHash).authorization) // assumes that chain is in local event graph
+    else capHash +: authorizationChain(events(capHash)._1.authorization) // assumes that chain is in local event graph
   }
 
 }
+
+enum CausalOrder:
+    case BEFORE
+    case AFTER
+    case CONCURRENT
+    case EQUAL
+    case UNKNOWN
 
 object Authorization {
   def materialize[T](eventGraph: EventGraph[T], deltaValueStore: DeltaValueStore[T]): T = ???
