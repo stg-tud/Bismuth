5bb56c35e0fc62e980f21d235062071f6d090e74
Christian Kuessner <17302779+ckuessner@users.noreply.github.com>
2026-07-16 14:55:33 +0200

    Add materialize function

--- Modules/Channels/jvm/src/main/scala/replication/authz/Authorization.scala
+++ Modules/Channels/jvm/src/main/scala/replication/authz/Authorization.scala
@@ -3,29 +3,67 @@ package replication.authz
 import com.github.plokhotnyuk.jsoniter_scala.core.{JsonValueCodec, readFromArray}
 import crypto.Commitment.RevealedValue
 import crypto.{Hash, PublicIdentity}
+import rdts.base.{Bottom, Lattice}
 import rdts.filters.Filter
 import replication.authz.ArdtEvent.Payload.{Capability, DeltaCommitment}
 
 object Authorization {
-  def materialize[T](eventGraph: EventGraph[T], deltaValueStore: DeltaValueStore[T]): T = ???
+  def materialize[T: {Lattice, Bottom, JsonValueCodec, Filter}](
+      eventGraph: EventGraph[T],
+      deltaValueStore: DeltaValueStore[T]
+  ): T = {
+    eventGraph.events.iterator.foldLeft(Bottom[T].empty) {
+      case (left, (deltaEventHash, (deltaEvent @ ArdtEvent(deltaCommitment: DeltaCommitment, _, _, _, _), _))) =>
+        deltaValueStore.get(deltaCommitment.commitment)
+          .map(commited => readFromArray[T](commited.value))
+          .filter(rdt => mayWrite(eventGraph, deltaEventHash, deltaEvent, rdt))
+          .map(left.merge)
+          .getOrElse(left)
+      case (left, _) => left
+    }
+  }
 
-  def mayRead[T](replica: PublicIdentity, delta: Hash, eventGraph: EventGraph[T]): Option[Boolean] = ???
+  def mayRead[T](replica: PublicIdentity, delta: Hash, eventGraph: EventGraph[T]): Option[Boolean] =
+    ???
 
   def mayWrite[T: {JsonValueCodec, Filter}](
       eventGraph: EventGraph[T],
       deltaEventHash: Hash,
       deltaValue: RevealedValue
   ): Boolean = {
-    // Delegation validity and validity of capability use are invariants of eventgraph, thus not checked here
     val (deltaEvent, _) = eventGraph.events(deltaEventHash)
-    (deltaEvent.payload, eventGraph.events(deltaEvent.authorization)._1.payload) match {
-      case (DeltaCommitment(commitment), Capability(_, _, write)) =>
-        val delta       = readFromArray[T](deltaValue.value)
+    mayWrite(eventGraph, deltaEventHash, deltaEvent, deltaValue)
+  }
+
+  private def mayWrite[T: {JsonValueCodec, Filter}](
+      eventGraph: EventGraph[T],
+      deltaEventHash: Hash,
+      deltaEvent: ArdtEvent,
+      revealedValue: RevealedValue
+  ): Boolean = {
+    deltaEvent.payload match {
+      case DeltaCommitment(commitment) =>
+        if revealedValue.commitment != commitment then return false
+        val delta = readFromArray[T](revealedValue.value)
+        mayWrite(eventGraph, deltaEventHash, deltaEvent, delta)
+      case _ => throw IllegalArgumentException(s"$deltaEvent is not a delta commitment")
+    }
+  }
+
+  private def mayWrite[T: {JsonValueCodec, Filter}](
+      eventGraph: EventGraph[T],
+      deltaEventHash: Hash,
+      deltaEvent: ArdtEvent,
+      delta: T
+  ): Boolean = {
+    // Delegation & revocation validity and validity of capability use are invariants of eventgraph, thus not checked here
+    eventGraph.events(deltaEvent.authorization)._1.payload match {
+      case Capability(_, _, write) =>
         val revocations =
           eventGraph
             .revocations(deltaEvent.authorization)
             .filter(revocation => !eventGraph.causallyBefore(deltaEventHash, revocation))
-        Filter[T].isAllowed(delta, write) && deltaValue.commitment == commitment && revocations.isEmpty
+        Filter[T].isAllowed(delta, write) && revocations.isEmpty
       case _ => false
     }
   }
