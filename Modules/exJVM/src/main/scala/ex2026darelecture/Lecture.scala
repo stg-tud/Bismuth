6068f2b288e09b2bf2fb901111a8a47a3deebf0b
ragnar <git@rmgk.de>
2026-09-03 18:48:50 +0200

    map voting

--- Modules/exJVM/src/main/scala/ex2026darelecture/Lecture.scala
+++ Modules/exJVM/src/main/scala/ex2026darelecture/Lecture.scala
@@ -1,6 +1,9 @@
 package ex2026darelecture
 
-import rdts.base.Lattice
+import rdts.base.LocalUid.replicaId
+import rdts.base.{Lattice, LocalUid, Uid}
+import rdts.protocols.{BallotNum, Participants}
+import rdts.protocols.Participants.participants
 
 object Lecture {
 
@@ -22,4 +25,35 @@ object Lecture {
       given Lattice[Sum.F.type] = Lattice.derived
 
       Lattice.sumLattice
+
+  case class Voting[A](votes: Map[Uid, A] = Map.empty[Uid, A]) {
+    def threshold(using Participants): Int = participants.size / 2 + 1
+
+    def decision(using Participants): Option[A] =
+      leadingCount match
+          case Some((v, count)) if count >= threshold => Some(v)
+          case _                                      => None
+
+    def voteFor(v: A)(using LocalUid, Participants): Voting[A] =
+      if !participants.contains(replicaId) || votes.contains(replicaId)
+      then Voting() // already voted!
+      else
+          Voting(Map(replicaId -> v))
+
+    def leadingCount: Option[(A, Int)] =
+        val grouped: Map[A, Int] = votes.values.groupBy(identity).map((value, vts) => (value, vts.size))
+        grouped.maxByOption((_, size) => size)
+  }
+
+  type LeaderElection = Voting[Uid]
+
+  case class PaxosRound[A](
+      leaderElection: LeaderElection = Voting(),
+      proposals: Voting[A] = Voting()
+  )
+
+  case class Paxos[A](
+      rounds: Map[BallotNum, PaxosRound[A]] = Map.empty[BallotNum, PaxosRound[A]]
+  )
+
 }
