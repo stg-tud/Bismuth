33d3c4a483dffc843ab299686efcca9b1162d0f7
ragnar <git@rmgk.de>
2026-09-04 10:36:19 +0200

    extract voting

--- Modules/exJVM/src/main/scala/ex2026darelecture/Lecture.scala
+++ Modules/exJVM/src/main/scala/ex2026darelecture/Lecture.scala
@@ -27,34 +27,4 @@ object Lecture {
 
       Lattice.sumLattice
 
-  case class Voting[A](votes: Map[Uid, A] = Map.empty[Uid, A]) {
-    def threshold(using Participants): Int = participants.size / 2 + 1
-
-    def decision(using Participants): Agreement[A] =
-      leadingCount match
-          case Some((v, count)) if count >= threshold => Agreement.Decided(v)
-          case _                                      => Agreement.Undecided
-
-    def voteFor(v: A)(using LocalUid, Participants): Voting[A] =
-      precondition(participants.contains(replicaId) && !votes.contains(replicaId)):
-          Voting(Map(replicaId -> v))
-
-    def leadingCount: Option[(A, Int)] =
-        val grouped: Map[A, Int] = votes.values.groupBy(identity).map((value, vts) => (value, vts.size))
-        grouped.maxByOption((_, size) => size)
-  }
-
-  given [A]: Bottom[Voting[A]] = Bottom.provide(Voting())
-
-  type LeaderElection = Voting[Uid]
-
-  case class PaxosRound[A](
-      leaderElection: LeaderElection = Voting(),
-      proposals: Voting[A] = Voting()
-  )
-
-  case class Paxos[A](
-      rounds: Map[BallotNum, PaxosRound[A]] = Map.empty[BallotNum, PaxosRound[A]]
-  )
-
 }
---
+++ Modules/exJVM/src/main/scala/ex2026darelecture/Voting.scala
@@ -0,0 +1,41 @@
+package ex2026darelecture
+
+import rdts.base.LocalUid.replicaId
+import rdts.base.{Bottom, Lattice, LocalUid, Uid}
+import rdts.protocols.Participants.participants
+import rdts.protocols.Util.*
+import rdts.protocols.{BallotNum, Participants}
+
+case class Voting[A](votes: Map[Uid, A] = Map.empty[Uid, A]) {
+  def threshold(using Participants): Int = participants.size / 2 + 1
+
+  def decision(using Participants): Agreement[A] =
+    leadingCount match
+        case Some((v, count)) if count >= threshold => Agreement.Decided(v)
+        case _                                      => Agreement.Undecided
+
+  def voteFor(v: A)(using LocalUid, Participants): Voting[A] =
+    precondition(participants.contains(replicaId) && !votes.contains(replicaId)):
+        Voting(Map(replicaId -> v))
+
+  def leadingCount: Option[(A, Int)] =
+      val grouped: Map[A, Int] = votes.values.groupBy(identity).map((value, vts) => (value, vts.size))
+      grouped.maxByOption((_, size) => size)
+}
+
+object Voting {
+  given [A] => Bottom[Voting[A]] = Bottom.provide(Voting())
+  given [A] => Lattice[Voting[A]] = {
+    given Lattice[A] = Lattice.assertEquals
+    Lattice.derived
+  }
+}
+
+type LeaderElection = Voting[Uid]
+
+case class PaxosRound[A](
+    leaderElection: LeaderElection = Voting(),
+    proposals: Voting[A] = Voting()
+)
+
+case class Paxos[A](rounds: Map[BallotNum, PaxosRound[A]] = Map.empty[BallotNum, PaxosRound[A]])
