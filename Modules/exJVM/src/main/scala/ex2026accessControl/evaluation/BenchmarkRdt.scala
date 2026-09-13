de275ef302e2add3e0ea45f769f484485fe0c01d
Christian Kuessner <17302779+ckuessner@users.noreply.github.com>
2026-09-13 23:44:34 +0200

    Add more stuff for benchmark rdt

--- Modules/exJVM/src/main/scala/ex2026accessControl/evaluation/BenchmarkRdt.scala
+++ Modules/exJVM/src/main/scala/ex2026accessControl/evaluation/BenchmarkRdt.scala
@@ -5,31 +5,45 @@ import rdts.datatypes.{LastWriterWins, PosNegCounter}
 import rdts.filters.Filter
 
 case class BenchmarkRdt(
-    title: LastWriterWins[Int],
-    nestedA: MultipleFields,
-    nestedB: MultipleFields,
-    moreNesting: MoreNesting
+    a: LastWriterWins[Int],
+    b: MultipleFields,
+    c: MoreNesting,
+    d: EvenMoreNesting
 )
 
-case class MultipleFields(counterA: PosNegCounter, counterB: PosNegCounter, lastWriterWins: LastWriterWins[Int])
+case class MultipleFields(x: PosNegCounter, y: PosNegCounter, z: LastWriterWins[Int])
 
-case class MoreNesting(something: MultipleFields, other: PosNegCounter)
+case class MoreNesting(alpha: MultipleFields, beta: PosNegCounter)
 
-case class EvenMoreNesting(moreNesting: MoreNesting)
+case class EvenMoreNesting(one: MoreNesting, two: LastWriterWins[Int])
 
 object BenchmarkRdt {
-  given Lattice[BenchmarkRdt]   = Lattice.derived
-  given Lattice[MultipleFields] = Lattice.derived
-  given Lattice[MoreNesting]    = Lattice.derived
+  given Lattice[BenchmarkRdt]    = Lattice.derived
+  given Lattice[MultipleFields]  = Lattice.derived
+  given Lattice[MoreNesting]     = Lattice.derived
+  given Lattice[EvenMoreNesting] = Lattice.derived
 
-  given Bottom[Int]            = Bottom.provide(0)
-  given Bottom[BenchmarkRdt]   = Bottom.derived
-  given Bottom[MultipleFields] = Bottom.derived
-  given Bottom[MoreNesting]    = Bottom.derived
+  given Bottom[Int]             = Bottom.provide(0)
+  given Bottom[BenchmarkRdt]    = Bottom.derived
+  given Bottom[MultipleFields]  = Bottom.derived
+  given Bottom[MoreNesting]     = Bottom.derived
+  given Bottom[EvenMoreNesting] = Bottom.derived
 
-  given Filter[BenchmarkRdt] = Filter.derived
+  given Filter[BenchmarkRdt]        = Filter.derived
   given Filter[LastWriterWins[Int]] = Filter.terminalLwwFilter
-  given Filter[PosNegCounter] = Filter.ofTerminalValue
-  given Filter[MultipleFields] = Filter.derived
-  given Filter[MoreNesting] = Filter.derived
+  given Filter[PosNegCounter]       = Filter.ofTerminalValue
+  given Filter[MultipleFields]      = Filter.derived
+  given Filter[MoreNesting]         = Filter.derived
+  given Filter[EvenMoreNesting]     = Filter.derived
+
+  private val multiFieldsPerms       = List("x", "y", "z")
+  private val moreNestingPerms       = List("alpha", for { f <- multiFieldsPerms } yield s"alpha.$f", "beta")
+  private val evenMoreNestingPerms   = List("one", for { f <- moreNestingPerms } yield s"one.$f", "two")
+  val benchmarkRdtPerms: Seq[String] =
+    Seq("a", "b")
+    ++ (for { f <- multiFieldsPerms } yield s"b.$f")
+    ++ Seq("c")
+    ++ (for { f <- moreNestingPerms } yield s"c.$f")
+    ++ Seq("d")
+    ++ (for { f <- evenMoreNestingPerms } yield s"d.$f")
 }
